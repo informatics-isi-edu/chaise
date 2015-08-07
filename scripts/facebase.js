f9aacecbf5ea703c69c510787c22db696ad6f80b
Serban Voinea <serban@isi.edu>
2015-08-07 11:13:44 -0700

    Removed obsolete file facebase.js.

--- scripts/facebase.js
+++
@@ -1,7 +0,0 @@
-function loadApplicationHeaderAndFooter() {
-}
-
-function initApplicationHeader(tables) {
-	$('#headerSearch').keyup(function(event) {$('#dataSearch').val($('#headerSearch').val());$('#dataSearch').change();});
-}
-
