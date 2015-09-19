2f09426c1c0165332528838eefce17673b03a0eb
Bennett Lee <bennett@tapiture.com>
2015-09-19 10:39:24 -0700

    switch config to point to vm 030

--- chaise-config.js
+++ chaise-config.js
@@ -3,5 +3,6 @@
 var chaiseConfig = {
 		authnProvider: 'goauth',
 		feedbackURL: 'http://goo.gl/forms/f30sfheh4H',
-		helpURL: '/help/using-the-data-browser/'
+		helpURL: '/help/using-the-data-browser/',
+        ermrestLocation: 'https://vm-dev-030.misd.isi.edu/'
 }
