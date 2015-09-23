ddabe0a1916f06eacfc1812b3dadd95f8645a77e
Jessie Wong <jessie@isi.edu>
2015-09-22 23:28:44 -0700

    Fixed sidebar toggling; added button to show sidebar

--- scripts/facetsService.js
+++ scripts/facetsService.js
@@ -8,11 +8,11 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 	this.display = function (table, column) {
 		return COLUMNS_ALIAS[table] != null ? COLUMNS_ALIAS[table][column] : '';
 	};
-	
+
 	this.html = function (table, column, data) {
 		return hasAnnotation(table, column, 'html') ? $sce.trustAsHtml(data) : data;
 	};
-	
+
 	this.setSortOption = function () {
 		var sortOption = FacetsData.sortInfo;
 		if (sortOption != null && sortOption['fields'].length > 1) {
@@ -20,7 +20,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		}
 		FacetsData['sortOption'] = sortOption;
 	};
-	
+
 	this.successSearchFacets = function (data, totalItems, page, pageSize) {
 		FacetsData.ermrestData = data;
 		FacetsData.collectionsPredicate = getCollectionsPredicate(FacetsData.entityPredicates, FacetsData);
@@ -34,7 +34,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		}
 		FacetsData.progress = false;
 	};
-	
+
 	this.initTable = function () {
 		$('footer').hide();
 		$('#headerSearch').val('');
@@ -68,14 +68,14 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		this.initPageRange();
 		clearFacets(FacetsData);
 	};
-	
+
 	this.initPageRange = function () {
 	    for (var i = 1; i <= FacetsData.tagPages; i++) {
 	    	FacetsData.pageRange.push(i);
 	    	FacetsData.pageMap[i] = i;
 	    }
 	};
-	
+
 	this.updateSessionFilter = function () {
 		emptyJSON(FacetsData.sessionFilters);
     	$.each(FacetsData.facets, function(i, facet) {
@@ -87,44 +87,27 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
     		}
     	});
 	};
-	
+
 	this.sidebarClick = function (toggle) {
 	    var overlay = $('.sidebar-overlay');
-	    
 	    if (toggle == 'sidebar-toggle') {
-	      //  var sidebar = $('#sidebar');
-	      //  sidebar.toggleClass('open');
-	          var sidebar = $('.sidebar');
-	          var main = $('.main');
-	          var openbtn = $('.open-side');
-	          sidebar.removeClass('open');
-	          main.removeClass('col-md-9');
-	          main.addClass('col-md-12');
-	          openbtn.removeClass('hidden');
-	   // } else if (toggle == 'sidebar-open') {
-	   //     var sidebar = $('#sidebar');
-	   //       var main = $('.main');
-	   //       var openbtn = $('.open-side');
-	   //       sidebar.addClass('open');
-	   //       main.removeClass('col-md-12');
-	   //       main.addClass('col-md-8');
-	   //       openbtn.addClass('hidden');   
+				$('#sidebar').toggleClass('open');
+				var main = $('.main');
+				main.toggleClass('col-md-9 col-md-12');
+				$('div.open-side').toggleClass('hidden show');
 	    } else if (toggle == 'field-toggle') {
-	        var sidebar = $('#editfilter');
-	        sidebar.toggleClass('open');
+        $('#editfilter').toggleClass('open');
 	    } else if (toggle == 'collections-toggle') {
-	        var sidebar = $('#collectionsTree');
-	        sidebar.toggleClass('open');
+        $('#collectionsTree').toggleClass('open');
 	    } else if (toggle == 'more-field-toggle') {
 	    	if (FacetsData.facetSelection) {
 		    	this.updateSessionFilter();
 	    	}
-	        var sidebar = $('#morefilters');
-	        sidebar.toggleClass('open');
+        $('#morefilters').toggleClass('open');
 	    }
 
 	};
-	
+
 	this.initSortOption = function () {
 		$.each(FacetsData.colsDefs, function(i, col) {
 			if (isSortable(FacetsData.table, col.field)) {
@@ -132,7 +115,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 			}
 		});
 	};
-	
+
 	this.predicate = function (facet, keyCode, successSearchFacets, successUpdateModels) {
 		if (FacetsData.box[facet['table']][facet['name']]['value'] == '') {
 			FacetsData.facetClass[facet['table']][facet['name']] = '';
@@ -143,7 +126,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		FacetsData.pagingOptions.currentPage = 1;
 		getErmrestData(FacetsData, successSearchFacets, successUpdateModels);
 	};
-	
+
 	this.predicate_slider = function (facet, successSearchFacets, successUpdateModels) {
 		if (FacetsData.box[facet['table']][facet['name']]['min'] > FacetsData.box[facet['table']][facet['name']]['floor']) {
 			FacetsData.box[facet['table']][facet['name']]['left'] = true;
@@ -160,7 +143,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		FacetsData.pagingOptions.currentPage = 1;
 		getErmrestData(FacetsData, successSearchFacets, successUpdateModels);
 	};
-	
+
 	this.successGetErmrestData = function (data, totalItems, page, pageSize) {
 		if (page == 1) {
 			FacetsData.ermrestData = (FacetsData.filter == null) ? data : [];
@@ -178,25 +161,25 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		}
 		FacetsData.progress = false;
 	};
-	
+
 	this.successGetMetadata = function (data, textStatus, jqXHR, successGetTableColumns) {
 		FacetsData['metadata'] = data;
 		getTableColumns(FacetsData, successGetTableColumns);
 	};
-	
+
 	this.successGetTableColumns = function (columns) {
 		FacetsData['facets'] = columns['facets'];
 		FacetsData['colsDefs'] = columns['colsDefs'];
 		this.initSortOption();
 	};
-	
+
 	this.hide = function (facet) {
-		return (FacetsData.narrow[facet['table']][facet['name']] == null || !FacetsData.chooseColumns[facet['table']][facet['name']] || 
-				(FacetsData.box[facet['table']][facet['name']]['facetcount'] == 0 && 
+		return (FacetsData.narrow[facet['table']][facet['name']] == null || !FacetsData.chooseColumns[facet['table']][facet['name']] ||
+				(FacetsData.box[facet['table']][facet['name']]['facetcount'] == 0 &&
 						(FacetsData.colsDescr[facet['table']][facet['name']]['type'] == 'slider' ||
 								FacetsData.colsDescr[facet['table']][facet['name']]['type'] == 'enum' && !hasCheckedValues(FacetsData.box, facet))));
 	};
-	
+
 	this.if_type = function (facet, facet_type) {
 		var ret = false;
 		if (facet != null && FacetsData.colsDescr[facet['table']] != null && FacetsData.colsDescr[facet['table']][facet['name']] != null) {
@@ -211,21 +194,21 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		}
 		return ret;
 	};
-	
+
 	this.predicate_checkbox = function (facet, successSearchFacets, successUpdateModels) {
 		setFacetClass(FacetsData, facet, FacetsData.facetClass);
 		this.setSortOption();
 		FacetsData.pagingOptions.currentPage = 1;
 		getErmrestData(FacetsData, successSearchFacets, successUpdateModels);
 	};
-	
+
 	this.showFacetCount = function (facet) {
-		return (FacetsData.chooseColumns[facet['table']][facet['name']] && 
-				(FacetsData.box[facet['table']][facet['name']] != null && FacetsData.box[facet['table']][facet['name']]['facetcount'] > 0 || 
+		return (FacetsData.chooseColumns[facet['table']][facet['name']] &&
+				(FacetsData.box[facet['table']][facet['name']] != null && FacetsData.box[facet['table']][facet['name']]['facetcount'] > 0 ||
 						FacetsData.colsDescr[facet['table']] != null && FacetsData.colsDescr[facet['table']][facet['name']] != null &&
 						FacetsData.colsDescr[facet['table']][facet['name']]['type'] == 'enum' && hasCheckedValues(FacetsData.box, facet)));
 	};
-	
+
 	this.getEntityResults = function (event, data, successGetMetadata) {
 		var label = $(event.target).is('label') ? $(event.target) : $(event.target).parent();
 		var isNewSchema = (SCHEMA != data.schema);
@@ -319,7 +302,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 			}
 		}
 	};
-	
+
 	this.getFacetValues = function getFacetValues(facet) {
 		var value = FacetsData.box[facet['table']][facet['name']];
 		var values = [];
@@ -330,7 +313,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		});
 		return values;
 	};
-	
+
 	this.displayTitle = function displayTitle(facet) {
 		var values = this.getFacetValues(facet);
 		var ret = '';
@@ -342,7 +325,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 		});
 		return ret;
 	};
-	
+
 	this.setCollectionChiclets = function setCollectionChiclets(data, isNewSchema, that) {
 		var oldRoot = null;
 		var newRoot = null;
@@ -364,7 +347,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 			}
 			newRoot = newRootParent.name;
 		}
-		
+
 		var isNewRoot = (oldRoot != newRoot);
 
 		if (isNewSchema || isNewRoot) {
@@ -422,7 +405,7 @@ facetsService.service('FacetsService', ['$sce', 'FacetsData', function($sce, Fac
 			}
 		}
 	}
-	
+
 	this.showChiclet = function showChiclet(facet) {
 		var facet_type = null;
 		if (this.if_type(facet, 'slider')) {
