7ffca3bbfc3d7c3ca32c74b939012e8f451af21e
bennett <bennett@vm-dev-030.misd.isi.edu>
2015-09-25 10:47:49 -0700

    renamed detail folder to record, updated js to point to record page

--- chaise-config.js
+++ chaise-config.js
@@ -4,5 +4,5 @@ var chaiseConfig = {
 		authnProvider: 'goauth',
 		feedbackURL: 'http://goo.gl/forms/f30sfheh4H',
 		helpURL: '/help/using-the-data-browser/',
-        ermrestLocation: 'https://vm-dev-030.misd.isi.edu/'
+        ermrestLocation: 'https://vm-dev-030.misd.isi.edu'
 }
