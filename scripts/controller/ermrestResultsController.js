b8e3b8540fc12ae49c08e78485a91527dd4430e0
Bennett Lee <bennett@isi.edu>
2015-09-26 00:42:36 -0700

    update

--- scripts/controller/ermrestResultsController.js
+++ scripts/controller/ermrestResultsController.js
@@ -58,7 +58,7 @@ ermResultsController.controller('ResultsListCtrl', ['$scope', '$window', '$timeo
     // Returns the path for a given row
     this.rowPath = function(row){
 
-        var detailPath = window.location.origin + '/chaise/record/#!' + CATALOG + '/' + SCHEMA + ':' +  $scope.FacetsData.table + '/' + this.buildPredicate(PRIMARY_KEY, row);
+        var detailPath = window.location.origin + '/chaise/record/#' + CATALOG + '/' + SCHEMA + ':' +  $scope.FacetsData.table + '/' + this.buildPredicate(PRIMARY_KEY, row);
         return detailPath;
     };
 
@@ -67,8 +67,7 @@ ermResultsController.controller('ResultsListCtrl', ['$scope', '$window', '$timeo
 
 		event.preventDefault();
 
-        var detailPath = window.location.origin + '/chaise/record/#' + CATALOG + '/' + SCHEMA + ':' +  $scope.FacetsData.table + '/' + this.buildPredicate(PRIMARY_KEY, row);
-        window.location.href = detailPath;
+        window.location.href = this.rowPath(row);
         return false;
 
 		if (row == null) {
