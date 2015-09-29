0a10083ed1e99ea42d37d6a10f83841a2b102938
Bennett Lee <bennett@isi.edu>
2015-09-29 10:57:24 -0700

    remove chaise config

--- chaise-config.js
+++ chaise-config.js
@@ -3,6 +3,5 @@
 var chaiseConfig = {
 		authnProvider: 'goauth',
 		feedbackURL: 'http://goo.gl/forms/f30sfheh4H',
-		helpURL: '/help/using-the-data-browser/',
-        ermrestLocation: 'https://vm-dev-030.misd.isi.edu'
+		helpURL: '/help/using-the-data-browser/'
 }
