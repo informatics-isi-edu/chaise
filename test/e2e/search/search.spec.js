e1bfd2d0b8a1b99f82929713040b2939172e57de
Jessie Wong <jessie@isi.edu>
2015-10-16 17:39:09 -0700

    More tests

--- test/e2e/search/search.spec.js
+++ test/e2e/search/search.spec.js
@@ -1,9 +1,9 @@
 // Each suite should begin with a `describe`
 describe('In the Chaise search app,', function() {
-  beforeAll(function() {
-    browser.get('');
-  });
   describe('on load,', function() {
+    beforeAll(function() {
+      browser.get('');
+    });
     it('should show the spinner', function() {
       var spinner = element(by.id('spinner'));
       expect(spinner.isDisplayed()).toBe(true);
@@ -12,7 +12,6 @@ describe('In the Chaise search app,', function() {
       setTimeout(function() {
         var sidebar = element(by.id('sidebar'));
         expect(sidebar.isDisplayed()).toBeTruthy();
-        console.log('Do nothing');
       }, 800);
     });
     it('should have > 1 visible facets to choose from', function() {
@@ -24,24 +23,23 @@ describe('In the Chaise search app,', function() {
     });
   });
 
-  // describe('the initial facet selection sidebar', function() {
-  //   // Find a DOM element by its ng-model
-  //   var searchBox = element(by.model('FacetsData.searchFilter'));
-  //   it('should find > 1 attributes when searching for "Yang"', function() {
-  //     expect(searchBox.isPresent()).toBe(true);
-  //     // Set values via sendKeys();
-  //     searchBox.sendKeys('Yang');
-  //     // var attributes = element(by.)
-  //     // expect();
-  //   });
-  //   it('should find 0 attributes when searching for "1k23kjh3kl22lkj5jfsd"', function() {
-  //     expect(1).toEqual(1);
-  //   });
-  //   it('should reflect the current search results in initial sidebar heading', function() {
-  //     expect(1).toEqual(1);
-  //   });
-  // });
-
-
-
+  describe('the initial facet selection sidebar', function() {
+    var searchBox;
+    beforeAll(function() {
+      browser.get('');
+      // Find elements by ng-model
+      searchBox = element(by.model('FacetsData.searchFilter'));
+    });
+    it('should find > 1 attributes when searching for something that exists in db', function() {
+      setTimeout(function() {
+        expect(searchBox.isPresent()).toBe(true);
+        // Set values (usually inputs) via sendKeys();
+        searchBox.sendKeys('Yang');
+        var facets = element.all(by.repeater('facet in FacetsData.facets'));
+        var hiddenFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-hide'));
+        var numVisibleFacets = facets.count()-hiddenFacets.count();
+        expect(numVisibleFacets).toBeGreaterThan(0);
+      }, 800);
+    });
+  });
 });
