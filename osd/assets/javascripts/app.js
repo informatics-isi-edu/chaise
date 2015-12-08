25b0b94782297f8bda018bf4964e0399219394fd
jessie <jessie@isi.edu>
2015-12-07 21:13:24 -0800

    Parses Chaise URI to display image in OpenSeadragon viewer (informatics-isi-edu/rbk-project#22)

--- osd/assets/javascripts/app.js
+++ osd/assets/javascripts/app.js
@@ -2,8 +2,21 @@ var openSeadragonApp = angular.module('OpenSeadragonApp', []);
 
 // API to fetch data from ERMrest
 openSeadragonApp.service('Ermrest', ['$http', '$location', function($http, $location) {
-    this.getEntity = function getEntityURI(catalogId, schemaName, tableName, entityId) {
-        var entityPath = 'http://' + $location.host() + '/ermrest/catalog/' + catalogId + '/entity/' + schemaName + ':' + tableName + '/id=' + entityId;
+    var ERMREST_ENDPOINT = 'http://' + $location.host() + '/ermrest/catalog/';
+    if (chaiseConfig['ermrestLocation'] != null) {
+        ERMREST_ENDPOINT = chaiseConfig['ermrestLocation'] + '/ermrest/catalog';
+    }
+
+    // Parse Chaise url to determine required parameters to find the requested entity
+    var path = $location.path();
+    var params = path.split('/');
+    var catalogId = params[1];
+    var schemaName = params[2].split(':')[0];
+    var tableName = params[2].split(':')[1];
+    var entityId = params[3].split('=')[1];
+
+    this.getEntity = function getEntity() {
+        var entityPath = ERMREST_ENDPOINT + catalogId + '/entity/' + schemaName + ':' + tableName + '/id=' + entityId;
         return $http.get(entityPath).then(function(response) {
             if (response.data.length > 0) {
                 return response.data[0];
@@ -14,13 +27,10 @@ openSeadragonApp.service('Ermrest', ['$http', '$location', function($http, $loca
 
 // CONTROLLER
 openSeadragonApp.controller('MainController', ['$scope', 'Ermrest', function($scope, Ermrest) {
-    var catalogId = 1;
-    var entityId = 44;
-    var schemaName = 'rbk';
-    var tableName = 'image';
     $scope.viewerSource = '';
 
-    Ermrest.getEntity(catalogId, schemaName, tableName, entityId).then(function(data) {
+    // Fetch uri from image table
+    Ermrest.getEntity().then(function(data) {
         $scope.viewerSource = data.uri;
     });
 }]);
