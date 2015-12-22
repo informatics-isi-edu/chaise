57a38b92f6967a9efa9aa7b8153f39a8d609722d
Shuai Ma <mashuaihh@hotmail.com>
2015-12-22 10:26:43 -0800

    setup & starter test a,b

--- test/e2e/search/search.spec.js
+++ test/e2e/search/search.spec.js
@@ -1,59 +1,121 @@
 // Each suite should begin with a `describe`
-describe('In the Chaise search app,', function() {
+describe('In the Chaise search app,', function () {
   var EC = protractor.ExpectedConditions;
-  describe('on load,', function() {
-    beforeAll(function() {
+
+  describe('on load,', function () {
+    beforeAll(function () {
       browser.get('');
     });
-    it('should show the spinner', function(done) {
+    it('should show the spinner', function (done) {
       var spinner = element(by.id('spinner'));
       // Browser waits (up to 500ms) for spinner to become visible before continuing
-      browser.wait(EC.visibilityOf(spinner), 500).then(function() {
+      browser.wait(EC.visibilityOf(spinner), 500).then(function () {
         expect(spinner.isDisplayed()).toBe(true);
+        done();
       });
-      done();
     });
-    it('should open the initial sidebar', function(done) {
+
+    it('should open the initial sidebar', function (done) {
       var spinner = element(by.id('spinner'));
       var sidebar = element(by.id('sidebar'));
-      browser.wait(EC.visibilityOf(sidebar), 10000).then(function() {
+      browser.wait(EC.visibilityOf(sidebar), 10000).then(function () {
         expect(sidebar.isDisplayed()).toBe(true);
         expect(spinner.isDisplayed()).toBe(false);
+        done();
       });
-      done();
     });
-    it('should have > 1 visible facets to choose from', function(done) {
+
+    it('should have > 1 visible facets to choose from', function (done) {
       var facets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope'));
-      facets.then(function() {
+      facets.then(function () {
         expect(facets.count()).toBeGreaterThan(0);
       });
       var hiddenFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope.ng-hide'));
-      hiddenFacets.then(function() {
+      hiddenFacets.then(function () {
         expect(hiddenFacets.count()).toBeLessThan(facets.count());
+        done();
       });
-      done();
     });
   });
 
-  describe('the initial facet selection sidebar', function() {
-    it('should display 0 attributes when searching for something nonexistent', function(done) {
-      var searchBox = element(by.model('FacetsData.searchFilter'));
-      browser.wait(EC.visibilityOf(searchBox), 500).then(function() {
+  describe('the initial facet selection sidebar', function () {
+    var expectedFacetsNum = 0;
+    //choose the first element found, because it's the one we are looking for
+    //use first() instead of [0], so the Promise can be resolved
+    var searchBox = element.all(by.model('FacetsData.searchFilter')).first();
+    it('should display 0 attributes when searching for something nonexistent', function (done) {
+      browser.wait(EC.visibilityOf(searchBox), 500).then(function () {
         // Set values (usually inputs) via sendKeys();
-        searchBox.sendKeys('a;sd324ljr5858@#$%^&*(+-+-8+lkfjalk234j2;l543587dglij4r543df;iqw3');
+        searchBox.sendKeys('hello');
       });
-      setTimeout(function() {
-        var allFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope'));
-        allFacets.then(function() {
-          expect(allFacets.count()).toBeGreaterThan(0);
+
+      var allFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope'));
+      var hiddenFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope.ng-hide'));
+      //use then to get the resolved result of Promise count()
+      allFacets.count().then(function () {
+        //use then to get the resolved result of Promise count()
+        hiddenFacets.count().then(function () {
+          allFacets.count().then(function (allFacetsNum) {
+            hiddenFacets.count().then(function (hiddenFacetsNum) {
+              expect(allFacetsNum).toBeGreaterThan(0);
+              expect(hiddenFacetsNum).toBeGreaterThan(0);
+              var shownFacetsNum = allFacetsNum - hiddenFacetsNum;
+              expect(shownFacetsNum).toEqual(expectedFacetsNum);
+              done();
+            });
+          });
         });
-        var hiddenFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope.ng-hide'));
-        hiddenFacets.then(function() {
-          expect(hiddenFacets.count()).toBeGreaterThan(0);
+      });
+    });
+
+    it('should find 8 attributes when searching for \'RNA\' in the search box', function (done) {
+      var expectedAttrNum = 8;
+      browser.wait(EC.visibilityOf(searchBox), 500).then(function () {
+        // Set values (usually inputs) via sendKeys();
+        searchBox.clear();
+        searchBox.sendKeys('RNA');
+      });
+      var allFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope'));
+      var hiddenFacets = element.all(by.css('#sidebar ul.sidebar-nav li.ng-scope.ng-hide'));
+      //use then to get the resolved result of Promise count()
+      allFacets.count().then(function () {
+        //use then to get the resolved result of Promise count()
+        hiddenFacets.count().then(function () {
+          allFacets.count().then(function (allFacetsNum) {
+            hiddenFacets.count().then(function (hiddenFacetsNum) {
+              expect(allFacetsNum).toBeGreaterThan(0);
+              expect(hiddenFacetsNum).toBeGreaterThan(0);
+              var shownFacetsNum = allFacetsNum - hiddenFacetsNum;
+              expect(shownFacetsNum).toEqual(expectedAttrNum);
+              done();
+            });
+          });
         });
-        expect(allFacets.count()).toEqual(hiddenFacets.count());
-      }, 10000);
-      done();
+      });
+    });
+
+    it('should show 3 attributes after clicking \"Experiment Type\"', function (done) {
+      var expectedAttrNum = 3;
+      var searchBoxInput = searchBox.getAttribute('value');
+      expect(searchBoxInput).toBe('RNA');
+
+      var experimentFacet = element(by.cssContainingText('.field-toggle.ng-binding', 'Experiment Type'));
+      browser.wait(EC.visibilityOf(experimentFacet), 500).then(function () {
+        experimentFacet.click();
+      });
+      var allFacets = element.all(by.css('#editfilter ul.nav.filteritems li.ng-scope'));
+      var hiddenFacets = element.all(by.css('#editfilter ul.nav.filteritems li.ng-scope.ng-hide'));
+      //use then to get the resolved result of Promise count()
+      allFacets.count().then(function (allFacetsNum) {
+        //use then to get the resolved result of Promise count()
+        hiddenFacets.count().then(function (hiddenFacetsNum) {
+          expect(allFacetsNum).toBeGreaterThan(0);
+          expect(hiddenFacetsNum).toBeGreaterThan(0);
+          var shownFacets = allFacetsNum - hiddenFacetsNum;
+          expect(shownFacets).toBe(expectedAttrNum);
+          done();
+        });
+      });
     });
   });
 });
