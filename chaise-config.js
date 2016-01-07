3416ec295f5436fb97bc4d5593b8b7d1c496fd4e
Jessie Wong <jessie@thisisjessie.com>
2016-01-07 11:23:35 -0800

    Update and rename chaise-config.js to chaise-config-sample.js
    Create a sample file for developers to use in their own installations of Chaise

--- chaise-config.js
+++
@@ -1,9 +0,0 @@
-// Configure deployment-specific data here
-
-var chaiseConfig = {
-	authnProvider: 'goauth',
-	feedbackURL: 'http://goo.gl/forms/f30sfheh4H',
-	helpURL: '/help/using-the-data-browser/',
-	showBadgeCounts: false,
-    recordResource: '/viewer/'
-}
