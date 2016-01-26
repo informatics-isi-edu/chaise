c81502ef2f96ce4626a2dfe30b6581dde75dc0b4
Jessie Wong <jessie@thisisjessie.com>
2016-01-25 17:07:57 -0800

    Add update function to annotations service

--- viewer/v2/annotations/annotations.controller.js
+++ viewer/v2/annotations/annotations.controller.js
@@ -8,8 +8,23 @@
         vm.annotations = annotations;
         vm.anatomies = anatomies;
 
+        vm.editedAnnotation = null; // Track which annotation is being edited right now; used to show/hide the right UI elements depending on which one is being edited.
+        // vm.originalAnnotation = null; // Holds the original contents of annotation in the event that a user cancels an edit
 
+        vm.editAnnotation = function editAnnotation(annotation) {
+            vm.editedAnnotation = annotation.data.id;
+            // vm.originalAnnotation = annotation;
+        };
 
+        vm.cancelAnnotationEdit = function cancelAnnotationEdit(annotation) {
+            vm.editedAnnotation = null;
+            // annotation = vm.originalAnnotation;
+        };
+
+        vm.updateAnnotation = function updateAnnotation(annotation) {
+            vm.editedAnnotation = null;
+            return AnnotationsService.updateAnnotation(annotation);
+        }
 
         vm.deleteAnnotation = function deleteAnnotation(annotation) {
             return AnnotationsService.deleteAnnotation(annotation);
--- viewer/v2/annotations/annotations.service.js
+++ viewer/v2/annotations/annotations.service.js
@@ -4,6 +4,15 @@
     angular.module('chaise.viewer')
 
     .factory('AnnotationsService', ['annotations', '$window', function(annotations, $window) {
+        var origin = window.location.origin;
+
+        function updateAnnotation(annotation) {
+            // Update in ERMrest
+            annotation.update();
+            // Update in Annotorious
+            $window.frames[0].postMessage({messageType: 'updateAnnotation', content: annotation.data}, origin);
+        }
+
         function deleteAnnotation(annotation) {
             // Delete from ERMrest
             annotation.delete();
@@ -11,10 +20,11 @@
             var index = annotations.indexOf(annotation);
             annotations.splice(index, 1);
             // Delete in Annotorious
-            $window.frames[0].postMessage({messageType: 'deleteAnnotation', content: annotation.data}, window.location.origin);
+            $window.frames[0].postMessage({messageType: 'deleteAnnotation', content: annotation.data}, origin);
         }
 
         return {
+            updateAnnotation: updateAnnotation,
             deleteAnnotation: deleteAnnotation
         };
     }]);
