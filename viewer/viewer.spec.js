f2b61f2b21227b45474ba499cdd15d490ab1330d
Jessie Wong <jessie@isi.edu>
2016-02-03 12:02:54 -0800

    Add unit tests

--- viewer/viewer.spec.js
+++ viewer/viewer.spec.js
@@ -1,19 +1,34 @@
 'use strict';
 
-describe('chaise.viewer module', function() {
+// FACTORY: AnnotationsService
+describe('AnnotationsService', function() {
+    var mockAnnotationsService = null;
+
     beforeEach(function() {
         angular.mock.module('chaise.viewer');
-    });
-
-    describe('AnnotationsService', function() {
-        var mockAnnotationsService = null;
-        beforeEach(inject(function(AnnotationsService) {
+        inject(function(AnnotationsService) {
             mockAnnotationsService = AnnotationsService;
-        }));
-        it('should have drawAnnotation() method', function() {
-            expect(mockAnnotationsService.drawAnnotation).toBeDefined();
         });
     });
+
+    it('should define a drawAnnotation() method', function() {
+        expect(mockAnnotationsService.drawAnnotation).toBeDefined();
+        expect(mockAnnotationsService.drawAnnotation).toEqual(jasmine.any(Function));
+    });
+
+    it('should define a cancelNewAnnotation() method', function() {
+        expect(mockAnnotationsService.cancelNewAnnotation).toBeDefined();
+        expect(mockAnnotationsService.cancelNewAnnotation).toEqual(jasmine.any(Function));
+    });
+
+    it('should define a highlightAnnotation() method', function() {
+        expect(mockAnnotationsService.highlightAnnotation).toBeDefined();
+        expect(mockAnnotationsService.highlightAnnotation).toEqual(jasmine.any(Function));
+    });
+
+    // TODO: Test the rest of the methods in the service
+
+    // TODO: Figure out how to mock the postMessages...
 });
 
 // FILTERS =====================================================================
@@ -45,5 +60,33 @@ describe('chaise.viewer filters', function() {
             expect(underscoreToSpace('chaise_is_AWEsome')).toEqual('chaise is AWEsome');
         });
     });
+});
 
+describe('SidebarSwitchController', function() {
+    var $controller;
+
+    beforeEach(function() {
+        angular.mock.module('chaise.viewer');
+        inject(function(_$controller_) {
+            $controller = _$controller_;
+        });
+    });
+
+    it('setSidebar() should set the sidebar', function() {
+        var controller = $controller('SidebarSwitchController');
+        expect(controller.sidebar).toEqual(controller.sidebars[0]);
+        controller.setSidebar('exampleSidebar');
+        expect(controller.sidebar).toEqual('exampleSidebar');
+    });
+});
+
+describe('OSDController', function() {
+    var $controller;
+
+    beforeEach(function() {
+        angular.mock.module('chaise.viewer');
+        inject(function(_$controller_) {
+            $controller = _$controller_;
+        });
+    });
 });
