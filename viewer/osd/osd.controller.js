de1fc21686ab36cf504436ed477948874f9ee296
Jessie Wong <jessie@isi.edu>
2016-02-05 23:29:03 -0800

    Fix Firefox bug where user needed to press Back twice to get back to previous page

--- viewer/osd/osd.controller.js
+++ viewer/osd/osd.controller.js
@@ -3,7 +3,8 @@
 
     angular.module('chaise.viewer')
 
-    .controller('OSDController', ['image', '$window', function OSDController(image, $window) {
+    // TODO: Remove this controller if not using it to fill in iframe
+    .controller('OSDController', ['image', function OSDController(image) {
         var vm = this;
         vm.image = image;
     }]);
