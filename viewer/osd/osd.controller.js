98c4f0ec632f533e1376c01df0f69eec31adba62
Jessie Wong <jessie@isi.edu>
2016-02-16 16:19:57 -0800

    Add openseadragon toolbar buttons

--- viewer/osd/osd.controller.js
+++ viewer/osd/osd.controller.js
@@ -5,20 +5,40 @@
 
     .controller('OSDController', ['image', function OSDController(image) {
         var vm = this;
+        var iframe = document.getElementById('osd').contentWindow;
+        var origin = window.location.origin;
         vm.image = image;
         vm.downloadView = downloadView;
+        vm.zoomInView = zoomInView;
+        vm.zoomOutView = zoomOutView;
+        vm.homeView = homeView;
+        vm.fullPageView = fullPageView;
 
         function downloadView() {
             var filename = vm.image.entity.data.slide_id.toString();
             if (!filename) {
                 filename = 'image';
             }
-            var iframe = document.getElementById('osd').contentWindow;
             iframe.postMessage({
                 messageType: 'downloadView',
                 content: filename
-            }, window.location.origin);
+            }, origin);
         }
 
+        function zoomInView() {
+            iframe.postMessage({messageType: 'zoomInView'}, origin);
+        }
+
+        function zoomOutView() {
+            iframe.postMessage({messageType: 'zoomOutView'}, origin);
+        }
+
+        function homeView() {
+            iframe.postMessage({messageType: 'homeView'}, origin);
+        }
+
+        function fullPageView() {
+            iframe.postMessage({messageType: 'fullPageView'}, origin);
+        }
     }]);
 })();
