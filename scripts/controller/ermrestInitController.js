cf384a04344162ebf81195793e838420abc2bac5
Serban Voinea <serban@isi.edu>
2016-02-25 16:35:28 -0800

    Added dynamic css loading in the search page when the customCSS key is defined in the chaise-config.js file.

--- scripts/controller/ermrestInitController.js
+++ scripts/controller/ermrestInitController.js
@@ -8,6 +8,14 @@ var ermInitController = angular.module('ermInitController', ['facetsModel', 'fac
 ermInitController.controller('InitListCtrl', ['$rootScope', '$scope', '$window', 'FacetsData', 'FacetsService', 'ermrest',
                                                       function($rootScope, $scope, $window, FacetsData, FacetsService, ermrest) {
 
+	if (chaiseConfig['customCSS'] !== undefined) {
+		var fileref = document.createElement("link");
+		fileref.setAttribute("rel", "stylesheet");
+		fileref.setAttribute("type", "text/css");
+		fileref.setAttribute("href", chaiseConfig['customCSS']);
+		document.getElementsByTagName("head")[0].appendChild(fileref);
+	}
+	
 	$('footer').hide();
 
 	$('.panel-collapse').on('hide.bs.collapse', function () {
