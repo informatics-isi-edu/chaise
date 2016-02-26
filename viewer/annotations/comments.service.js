d09ebb8e1c5433215eb3b9539d95487380226a3a
Jessie Wong <jessie@isi.edu>
2016-02-26 01:30:34 -0800

    Fix bug where annotation could not be deleted immediately after it was created

--- viewer/annotations/comments.service.js
+++ viewer/annotations/comments.service.js
@@ -31,8 +31,9 @@
                 // Not using the comment returned from ermrestClientFactory
                 // because it's incompatible with the comment objects loaded
                 // in viewer.module.js
-                var annotationId = newComment[0].annotation_id;
+                newComment[0].id = comment.data.id;
                 newComment[0].created = comment.data.created;
+                var annotationId = newComment[0].annotation_id;
                 if (!comments[annotationId]) {
                     comments[annotationId] = [];
                 }
@@ -49,6 +50,7 @@
                 var index = annotationComments.indexOf(comment);
                 annotationComments.splice(index, 1);
             }, function error(response) {
+                console.log(comment);
                 console.log(response);
             });
         }
