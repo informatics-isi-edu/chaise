3e88c31d42974c9b9f6e3741a35761a088a38f27
Jessie Wong <jessie@isi.edu>
2016-03-21 13:45:52 -0700

    Append .jpg to screenshot filename

--- viewer/osd/osd.controller.js
+++ viewer/osd/osd.controller.js
@@ -20,7 +20,7 @@
             }
             iframe.postMessage({
                 messageType: 'downloadView',
-                content: filename
+                content: filename + '.jpg'
             }, origin);
         }
 
