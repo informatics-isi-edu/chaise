fe19b6e56c7815979790d3f0fb527a521cce17d5
Jessie Wong <jessie@isi.edu>
2016-03-22 18:03:32 -0700

    Remove local version of ermrestjs

--- data-entry/ermrestjs/ngermrest.js
+++
@@ -1,6 +0,0 @@
-angular.module('ERMrest', [])
-
-    .factory('ermrestServerFactory', ['$http', '$q', function($http, $q) {
-        ERMrest.configure($http, $q);
-        return ERMrest.ermrestFactory;
-    }]);
