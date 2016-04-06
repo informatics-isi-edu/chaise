5c2357bdc96d77471bdae720e2a44015a5044422
Joshua Chudy <jchudy@Joshuas-MacBook-Pro.local>
2016-04-05 19:11:55 -0700

    scaffolding for later tests

---
+++ viewer/test/annotations.controller.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('AnnotationsController', function() {
+    var $controller;
+
+    beforeEach(function() {
+        angular.mock.module('chaise.viewer');
+        inject(function(_controller) {
+            $controller = _controller;
+        });
+    });
+
+    it('should have a lot of tests', function() {
+        
+    });
+});
---
+++ viewer/test/comments.controller.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('CommentsController', function() {
+    var $controller;
+
+    beforeEach(function() {
+        angular.mock.module('chaise.viewer');
+        inject(function(_controller) {
+            $controller = _controller;
+        });
+    });
+
+    it('should define a newComment', function() {
+
+    });
+
+    it('should be able to create a comment', function() {
+
+    });
+
+    it('should be able to get the number of comments', function() {
+
+    });
+
+    it('should be able to delete comments', function() {
+
+    });
+
+    it('should be able to reset comments', function() {
+
+    });
+});
