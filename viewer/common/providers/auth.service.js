1c2436fffc0890d6be168c65a22525895a5f92dd
Joshua Chudy <jchudy@excalibur.isi.edu>
2016-04-18 12:24:09 -0700

    undoing changes for editing comments

--- viewer/common/providers/auth.service.js
+++ viewer/common/providers/auth.service.js
@@ -43,15 +43,15 @@
             return false;
         }
 
-        function editComment(comment) {
-            if (user.role == 'curator') {
-                return true;
-            }
-            if (user.role == 'annotator' && isAuthor(comment.data.author, user.session)) {
-                return true;
-            }
-            return false;
-        }
+        // function editComment(comment) {
+        //     if (user.role == 'curator') {
+        //         return true;
+        //     }
+        //     if (user.role == 'annotator' && isAuthor(comment.data.author, user.session)) {
+        //         return true;
+        //     }
+        //     return false;
+        // }
 
         function deleteComment(comment) {
             if (user.role == 'curator') {
@@ -80,7 +80,7 @@
             editAnnotation: editAnnotation,
             deleteAnnotation: deleteAnnotation,
             createComment: createComment,
-            editComment: editComment,
+            // editComment: editComment,
             deleteComment: deleteComment,
             editMetadata: editMetadata
         };
