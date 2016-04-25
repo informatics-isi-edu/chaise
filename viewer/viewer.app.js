cf23200e78c743384417178c22f5ee1a867e5b8e
Jessie Wong <jessie@isi.edu>
2016-04-25 16:13:35 -0700

    Sort anatomies by ABC order

--- viewer/viewer.app.js
+++ viewer/viewer.app.js
@@ -203,6 +203,18 @@
                     for (var j = 0; j < length; j++) {
                         anatomies.push(_anatomies[j].data.term);
                     }
+                    anatomies.sort(function sortAnatomies(a, b) {
+                        a = a.toLowerCase();
+                        b = b.toLowerCase();
+                        if (a < b) {
+                            return -1;
+                        } else if (a > b) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+                    console.log('Anatomies:', anatomies);
                 }, function error(response) {
                     throw response;
                 });
