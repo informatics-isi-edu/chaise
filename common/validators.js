2e9d024f8a24cd9673eef640e195fbaf939b8b6b
Joshua Chudy <jchudy@isi.edu>
2016-05-12 17:42:54 -0700

    data input types and form validation

--- common/validators.js
+++ common/validators.js
@@ -28,5 +28,31 @@
                 };
             }
         };
+    })
+
+    // Validation directive for testing if an input value is a float
+    // Use: <input type="number" required float>
+    .directive('float', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+                ctrl.$validators.float = function(modelValue, viewValue) {
+                    if (ctrl.$isEmpty(modelValue)) {
+                        // consider empty models to be valid
+                        // use the `required` attribute in the HTML
+                        console.log("Empty");
+                        return true;
+                    }
+                    if (angular.isNumber(parseFloat(viewValue))) {
+                        // it is valid
+                        console.log("Passed REGEX");
+                        return true;
+                    }
+                    // it is invalid
+                    console.log("Invalid float value");
+                    return false;
+                };
+            }
+        };
     });
 })();
