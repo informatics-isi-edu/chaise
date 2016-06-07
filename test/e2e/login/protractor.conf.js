2b3a8c270b52e1a4822c519c5756c2519ff5142f
Chirag Sanghvi <csanghvi@appacitive.com>
2016-06-07 12:45:21 -0700

    Parameterized test data setup in protractor config and added globus login test

--- test/e2e/login/protractor.conf.js
+++ test/e2e/login/protractor.conf.js
@@ -3,7 +3,7 @@ exports.config = {
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   framework: 'jasmine2',
   capabilities: {
-    browserName: 'firefox',
+    browserName: 'chrome',
     'chromeOptions' : {
        args: ['--lang=en',
               '--window-size=1280,1024']
@@ -17,6 +17,7 @@ exports.config = {
     defaultTimeoutInterval: 30000
   },
   // If ng-app attribute is in a descendant <body>, tell Protractor where ng-app is
-  rootElement: '#loginApp',
+  //rootElement: '#loginApp',
+  rootElement: '#main-content',
   baseUrl: process.env.CHAISE_BASE_URL
 };
