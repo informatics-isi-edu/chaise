eef9261b72ba421256f6b07c8ebd75f4c436e5d6
Joshua Chudy <jchudy@isi.edu>
2016-07-08 11:50:14 -0700

    addressing Jessie's comments

--- record-two/record.controller.js
+++ record-two/record.controller.js
@@ -3,8 +3,8 @@
 
     angular.module('chaise.record')
 
-    .controller('RecordController', ['$rootScope', function FormController($rootScope) {
+    .controller('RecordController', ['$rootScope', function RecordController($rootScope) {
         // made this for manipulating code in html for record app
-        
+
     }]);
 })();
