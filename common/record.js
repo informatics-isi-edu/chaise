12cf1dd746b121af55fa3dedda154c9d6bdad85f
Joshua Chudy <jchudy@isi.edu>
2016-07-08 15:42:02 -0700

    Other changes Jessie suggested

--- common/record.js
+++ common/record.js
@@ -7,28 +7,14 @@
         return {
             restrict: 'E',
             transclude: true,
-            scope: {
-                record: '=',
-                table: '='
-            },
             templateUrl: '../common/templates/record.html',
             link: function(scope) {
-                scope.$watch('record', function(newVal, oldVal) {
-                    if(newVal) {
-                        scope.record = newVal;
-                    }
-                }, true);
 
-                scope.$watch('table', function(newVal, oldVal) {
-                    if(newVal) {
-                        scope.table = newVal;
-                        try {
-                            scope.columns = scope.table.columns.all();
-                        } catch (exception) {
-                            ErrorService.catchAll(exception);
-                        }
-                    }
-                });
+                try {
+                    scope.columns = scope.table.columns.all();
+                } catch (exception) {
+                    ErrorService.catchAll(exception);
+                }
             }
         };
     }]);
