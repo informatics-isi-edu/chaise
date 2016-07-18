020cf207aad259d71a5d9a4838a95de2c60cc486
Joshua Chudy <jchudy@isi.edu>
2016-07-18 15:00:13 -0700

    updated with cahnges to ermrestJS

--- record-two/record.app.js
+++ record-two/record.app.js
@@ -12,27 +12,29 @@
     ])
 
     // Config is no
-    .run(['ermrestServerFactory', 'UriUtils', 'ErrorService', '$log', '$rootScope', '$window', function runApp(ermrestServerFactory, UriUtils, ErrorService, $log, $rootScope, $window) {
+    .run(['ermrestServerFactory', 'UriUtils', 'ErrorService', '$log', '$rootScope', '$window', 'Session', function runApp(ermrestServerFactory, UriUtils, ErrorService, $log, $rootScope, $window, Session) {
 
-        UriUtils.setOrigin();
+        Session.getSession().then(function getSession(session) {
+            UriUtils.setOrigin();
+            return ermrestServerFactory.resolve($window.location);
+        }).then(function getReference(reference) {
+            $log.info(reference);
 
-        ermrestServerFactory.resolve($window.location).then(function success(reference) {
-            console.log(reference);
-
-            if (reference.filter) {
+            if (reference.filter && reference.table) {
+                $rootScope.table = reference.table;
                 var recordPath = new ERMrest.DataPath(reference.table);
                 var recordFilter = UriUtils.parsedFilterToERMrestFilter(reference.filter, reference.table);
 
                 recordPath.filter(recordFilter).entity.get().then(function success(record) {
-                    console.log(record);
+                    $log.info(record);
                     // So the data can be passed through the directive and watched for changes
                     $rootScope.record = record[0];
                 }, function error(response) {
                     throw reponse;
                 });
             }
-        }, function error() {
-
+        }, function error(response) {
+            $log.warn(response);
         }).catch(function genericCatch(exception) {
             ErrorService.catchAll(exception);
         });
