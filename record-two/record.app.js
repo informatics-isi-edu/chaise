5f1ca3addd921111df3a5847e9f9d040aa750440
Joshua Chudy <jchudy@isi.edu>
2016-07-28 16:20:51 -0700

    Record-two using contextualize

--- record-two/record.app.js
+++ record-two/record.app.js
@@ -24,6 +24,7 @@
         ERMrest.resolve(ermrestUri, {cid: context.appName}).then(function getReference(reference) {
             $log.info("Reference:", reference);
             $rootScope.reference = reference;
+            var recordReference = reference.contextualize.record;
 
             return reference.read(1);
         }).then(function getPage(page) {
