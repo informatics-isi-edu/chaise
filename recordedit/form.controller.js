60df35efb3cd1fdb9b1081d846d12de34b489d58
Chirag Sanghvi <csanghvi@appacitive.com>
2016-08-05 14:57:16 -0700

    added transformation of values while copying a row in recordedit

--- recordedit/form.controller.js
+++ recordedit/form.controller.js
@@ -110,21 +110,19 @@
          * Boolean: If the value is empty ('') then set it as null
          * Date/Timestamptz: If the value is empty ('') then set it as null
          */
-        function transformRowValues(model) {
-            model.rows.forEach(function(row) {
-                for (var k in row) {
-                    try {
-                        var column = model.table.columns.get(k);
-                        switch (column.type.name) {
-                            case 'boolean': if (row[k] == '') row[k] = null;
+        function transformRowValues(row, model) {
+            for (var k in row) {
+                try {
+                    var column = model.table.columns.get(k);
+                    switch (column.type.name) {
+                        case 'boolean': if (row[k] === '') row[k] = null;
+                                        break;
+                        case 'date':
+                        case 'timestamptz': if (row[k] === '') row[k] = null;
                                             break;
-                            case 'date':
-                            case 'timestamptz': if (row[k] == '') row[k] = null;
-                                                break;
-                        }
-                    } catch(e) {}
-                }
-            });
+                    }
+                } catch(e) {}
+            }
         }
 
         function submit() {
@@ -139,7 +137,9 @@
                 return;
             }
 
-            transformRowValues(model);
+            model.rows.forEach(function(row) {
+                transformRowValues(row, model);
+            });
 
             if (vm.editMode) {
                 model.table.entity.put(model.rows).then(function success(entities) {
@@ -174,7 +174,13 @@
             if (validRow) {
                 var rowset = vm.recordEditModel.rows;
                 var protoRow = rowset[index];
-                rowset.push(angular.copy(protoRow));
+                var row = angular.copy(protoRow);
+                
+                // transform row values to avoid parsing issues with null values
+                transformRowValues(row, vm.recordEditModel);
+                
+                rowset.push(row);
+
             }
         }
 
