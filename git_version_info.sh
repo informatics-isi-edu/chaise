a1c454fec851551ea468f251356c9029a811f38a
Chirag Sanghvi <csanghvi@appacitive.com>
2016-08-05 15:28:19 -0700

    Removed extra double quotes in git_version_info.sh

--- git_version_info.sh
+++ git_version_info.sh
@@ -20,17 +20,17 @@ DATE=$(date)
 
 TITLE="**************************CHAISE BUILD INFO******************************\n\nBUILD DATE : "$DATE
 
-CHAISECOMMIT="\n\nCHAISE : $(parse_git_branch)"\n"$(parse_git_hash)"
+CHAISECOMMIT="\n\nCHAISE : $(parse_git_branch)\n$(parse_git_hash)"
 
 cd ../ermrestjs
 
-ERMRESTJSCOMMIT="\n\nEMRESTJS : $(parse_git_branch)"\n"$(parse_git_hash)"
+ERMRESTJSCOMMIT="\n\nEMRESTJS : $(parse_git_branch)\n$(parse_git_hash)"
 
 
 if [ -n "$TRAVIS"]
 then
 cd ../ermrest
-ERMRESTCOMMIT="\n\nEMREST : $(parse_git_branch)"\n"$(parse_git_hash)"
+ERMRESTCOMMIT="\n\nEMREST : $(parse_git_branch)\n$(parse_git_hash)"
 else
 ERMRESTCOMMIT=""
 fi
