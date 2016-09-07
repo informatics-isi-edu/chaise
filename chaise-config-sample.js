c3034ec3ea034749d5cda0e00beed712f719ceae
howdyjessie <jessie@isi.edu>
2016-09-07 15:23:58 -0700

    Remove url logic from navbar menu directive; provide sample navbar menu The directive should not manipulate the url in anyway so that the data modeler can fully control what urls to link to in the navbar. A sample, commented-out navbar menu has been added to chaise-config-sample.js to help users.

--- chaise-config-sample.js
+++ chaise-config-sample.js
@@ -30,6 +30,34 @@ var chaiseConfig = {
     }
 };
 
+// Specify the layout of the navigation menu at the top of all Chaise apps
+var navbar_menu = [
+    // {
+    //     // This "Search" menu item has 2 nested dropdowns
+    //     // Use the "name" key to label your menu item
+    //     // Use the "children" key to specify dropdowns; you can nest as many dropdowns as you need.
+    //     name: "Search",
+    //     children: [
+    //         {
+    //             name: "Search 1",
+    //             children: [
+    //                 {
+    //                     name: "Search 1.1",
+    //                     url: "/chaise/search/#1/YOUR_CATALOG:YOUR_SCHEMA"
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // },
+    // {
+    //     // This "Create" menu item doesn't have any dropdowns.
+    //     // Use the "url" key to specify that this menu item's url and to signal that it doesn't have any chiilren.
+    //     // URLs can be absolute or relative to the document root.
+    //     name: "Create",
+    //     url: "/chaise/recordedit/#1/YOUR_CATALOG:YOUR_SCHEMA"
+    // }
+];
+
 if (typeof module === 'object' && module.exports && typeof require === 'function') {
     exports.config = chaiseConfig;
 }
