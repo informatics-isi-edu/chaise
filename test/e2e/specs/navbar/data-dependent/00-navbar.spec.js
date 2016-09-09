9eacbf309bec95f75e662fd9c931a75c83154254
howdyjessie <jessie@isi.edu>
2016-09-08 19:19:30 -0700

    Add test case

--- test/e2e/specs/navbar/data-dependent/00-navbar.spec.js
+++ test/e2e/specs/navbar/data-dependent/00-navbar.spec.js
@@ -1,13 +1,37 @@
 describe('Navbar ', function() {
-    var navbar, EC = protractor.ExpectedConditions;
+    var navbar, menu, EC = protractor.ExpectedConditions;
+    var expectedMenu = require(process.env.PWD + '/test/e2e/specs/navbar/data-dependent/chaise-config.js').navbarMenu;
 
-    beforeAll(function() {
-        navbar = $('#mainnav');
-        browser.wait(EC.presenceOf(navbar), 5000);
+    beforeAll(function (done) {
+        browser.get(browser.params.url || "");
+        // browser.get(process.env.CHAISE_BASE_URL + '/chaise-config.js');
+        navbar = element(by.id('mainnav'));
+        menu = element(by.id('navbar-menu'));
+        browser.wait(EC.presenceOf(navbar), 5000).then(function () {
+            done();
+        });
     });
 
     it('should be visible', function() {
         expect(navbar.isDisplayed()).toBeTruthy();
-        expect(1).toBe(1);
+    });
+
+    it('should have generate the right total # of list items', function() {
+        var nodesInDOM = navbar.element(by.id('navbar-menu')).all(by.tagName('li'));
+        var counter = 0;
+        function countNodes(array) {
+            array.forEach(function(element) {
+                counter++;
+                if (element.children) {
+                    countNodes(element.children);
+                }
+            });
+            return counter;
+        }
+        countNodes(expectedMenu);
+
+        nodesInDOM.count().then(function(count) {
+            expect(count).toEqual(counter);
+        });
     });
 });
