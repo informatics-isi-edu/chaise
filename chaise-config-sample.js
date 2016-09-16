3018da96fb79ed643b5df85be790c1612eda36b0
howdyjessie <jessie@isi.edu>
2016-09-16 15:44:52 -0700

    Add hierarhical scoping to newTab prop in navbar navbar_menu moved inside chaiseConfig. Data modelers can set the newTab property on any menu item inside chaiseConfig.navbarMenu and expect this setting to cascade down to descendants of that menu item. Removed anchor tag from profile name.

--- chaise-config-sample.js
+++ chaise-config-sample.js
@@ -27,38 +27,39 @@ var chaiseConfig = {
       extraAttribute: "Mouse Anatomic Source",
       chosenAttribute: "Data Type",
       chosenValue: "Expression microarray - gene"
+    },
+    newTab: true
+    navbarMenu: {
+        newTab: true,
+        children: [
+            // {
+            //     // This "Search" menu item has 2 nested dropdowns.
+            //     // Use the "name" key to label a menu item.
+            //     // Use the "children" key to specify dropdowns; you can nest as many dropdowns as you need.
+            //     name: "Search",
+            //     children: [
+            //         {
+            //             name: "Search 1",
+            //             children: [
+            //                 {
+            //                     name: "Search 1.1",
+            //                     url: "/chaise/search/#1/YOUR_CATALOG:YOUR_SCHEMA"
+            //                 }
+            //             ]
+            //         }
+            //     ]
+            // },
+            // {
+            //     // This "Create" menu item doesn't have any dropdowns.
+            //     // Use the "url" key to specify this menu item's url and to signal that it doesn't have any children.
+            //     // URLs can be absolute or relative to the document root.
+            //     name: "Create",
+            //     url: "/chaise/recordedit/#1/YOUR_CATALOG:YOUR_SCHEMA"
+            // }
+        ]
     }
 };
 
-// Specify the layout of the navigation menu at the top of all Chaise apps
-var navbar_menu = [
-    // {
-    //     // This "Search" menu item has 2 nested dropdowns.
-    //     // Use the "name" key to label a menu item.
-    //     // Use the "children" key to specify dropdowns; you can nest as many dropdowns as you need.
-    //     name: "Search",
-    //     children: [
-    //         {
-    //             name: "Search 1",
-    //             children: [
-    //                 {
-    //                     name: "Search 1.1",
-    //                     url: "/chaise/search/#1/YOUR_CATALOG:YOUR_SCHEMA"
-    //                 }
-    //             ]
-    //         }
-    //     ]
-    // },
-    // {
-    //     // This "Create" menu item doesn't have any dropdowns.
-    //     // Use the "url" key to specify this menu item's url and to signal that it doesn't have any children.
-    //     // URLs can be absolute or relative to the document root.
-    //     name: "Create",
-    //     url: "/chaise/recordedit/#1/YOUR_CATALOG:YOUR_SCHEMA"
-    // }
-];
-
 if (typeof module === 'object' && module.exports && typeof require === 'function') {
     exports.config = chaiseConfig;
-    exports.navbarMenu = navbar_menu;
 }
--- common/navbar.js
+++ common/navbar.js
@@ -2,6 +2,39 @@
     'use strict';
     angular.module('chaise.navbar', ['chaise.utils', 'chaise.authen'])
     .directive('navbar', ['$window', '$rootScope', 'Session', function($window, $rootScope, Session) {
+
+    // One-time transformation of chaiseConfig.navbarMenu to set the appropriate newTab setting at each node
+        var root = chaiseConfig.navbarMenu;
+        // Set default newTab property at root node
+        if (!root.hasOwnProperty('newTab')) {
+            root.newTab = true;
+            if (chaiseConfig.hasOwnProperty('newTab')) {
+                root.newTab = chaiseConfig.newTab;
+            }
+        }
+
+        var q = [root];
+
+        while (q.length > 0) {
+    	    var obj = q.shift();
+            var parentNewTab = obj.newTab;
+            // If current node is a leaf, do nothing
+            if (obj.url) {
+                continue;
+            }
+            // If current node has children, set each child's newTab to its own existing newTab or parent's newTab
+            for (var key in obj) {
+                if (key == 'children') {
+                    obj[key].forEach(function(child) {
+                        q.push(child);
+                        if (child.newTab === undefined) {
+                            child.newTab = parentNewTab;
+                        }
+                    });
+                }
+            }
+        }
+
         return {
             restrict: 'EA',
             scope: {},
@@ -10,7 +43,7 @@
                 scope.brandURL = chaiseConfig.navbarBrand;
                 scope.brandText = chaiseConfig.navbarBrandText || chaiseConfig.headTitle;
                 scope.brandImage = chaiseConfig.navbarBrandImage;
-                scope.menu = navbar_menu;
+                scope.menu = chaiseConfig.navbarMenu.children || [];
 
                 Session.getSession().then(function(session) {
                     $rootScope.session = session;
