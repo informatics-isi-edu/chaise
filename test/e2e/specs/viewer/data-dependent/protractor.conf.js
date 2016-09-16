f5ad0dd4e33059d5294a010336576ad5ef6731d6
howdyjessie <jessie@isi.edu>
2016-09-16 16:44:00 -0700

    Add configuration file for viewer app tests (#599)

--- test/e2e/specs/viewer/data-dependent/protractor.conf.js
+++ test/e2e/specs/viewer/data-dependent/protractor.conf.js
@@ -3,13 +3,13 @@ var pConfig = require('./../../../utils/protractor.configuration.js');
 var config = pConfig.getConfig({
 
     // Change this to your desired filed name and Comment below testConfiguration object declaration
-    // configFileName: 'viewer.dev.json',
+    configFileName: 'viewer.dev.json',
 
     /* Just in case if you plan on not giving a file for configuration, you can always specify a testConfiguration object
      * Comment above 2 lines
      * Empty configuration will run test cases against catalog 1 and default schema
-     */
      testConfiguration: {},
+     */
 
 
     /* If you want to set a custom base URL with some query string or other url parameters use
