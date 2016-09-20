457fe40d3324e7cdfc826200d4224bc4c6b055fb
howdyjessie <jessie@isi.edu>
2016-09-19 18:06:30 -0700

    Change default structure for navbar menu

--- common/navbar.js
+++ common/navbar.js
@@ -4,30 +4,32 @@
     .directive('navbar', ['$window', '$rootScope', 'Session', function($window, $rootScope, Session) {
 
     // One-time transformation of chaiseConfig.navbarMenu to set the appropriate newTab setting at each node
-        var root = chaiseConfig.navbarMenu;
-        // Set default newTab property at root node
-        if (!root.hasOwnProperty('newTab')) {
-            root.newTab = true;
-        }
-
-        var q = [root];
-
-        while (q.length > 0) {
-    	    var obj = q.shift();
-            var parentNewTab = obj.newTab;
-            // If current node is a leaf, do nothing
-            if (obj.url) {
-                continue;
+        var root = chaiseConfig.navbarMenu = chaiseConfig.navbarMenu || {};
+        if (root) {
+            // Set default newTab property at root node
+            if (!root.hasOwnProperty('newTab')) {
+                root.newTab = true;
             }
-            // If current node has children, set each child's newTab to its own existing newTab or parent's newTab
-            for (var key in obj) {
-                if (key == 'children') {
-                    obj[key].forEach(function(child) {
-                        q.push(child);
-                        if (child.newTab === undefined) {
-                            child.newTab = parentNewTab;
-                        }
-                    });
+
+            var q = [root];
+
+            while (q.length > 0) {
+                var obj = q.shift();
+                var parentNewTab = obj.newTab;
+                // If current node is a leaf, do nothing
+                if (obj.url) {
+                    continue;
+                }
+                // If current node has children, set each child's newTab to its own existing newTab or parent's newTab
+                for (var key in obj) {
+                    if (key == 'children') {
+                        obj[key].forEach(function(child) {
+                            q.push(child);
+                            if (child.newTab === undefined) {
+                                child.newTab = parentNewTab;
+                            }
+                        });
+                    }
                 }
             }
         }
