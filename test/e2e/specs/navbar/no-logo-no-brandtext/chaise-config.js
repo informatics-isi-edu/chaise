0b11633fb12f02c59eccb7b207d3a8f3e80f6e54
howdyjessie <jessie@thisisjessie.com>
2016-11-28 16:43:29 -0800

    Fix non-deterministic navbar tests (#766)

---
+++ test/e2e/specs/navbar/no-logo-no-brandtext/chaise-config.js
@@ -0,0 +1,69 @@
+/* This config tests navbar functionalities with:
+ - No navbarBrandImage
+ - No navbarBrandText
+ - No profileURL
+ - No signUpURL
+ - A specified headTitle
+*/
+
+
+var chaiseConfig = {
+    name: "navebar",
+    layout: 'list',
+    confirmDelete: true,
+    customCSS: '/assets/css/chaise.css',
+    navbarBrand: 'test123',
+    headTitle: 'show me on the navbar!',
+    logoutURL: 'test123',
+    dataBrowser: '/',
+    maxColumns: 6,
+    showUnfilteredResults: false,
+    defaultAnnotationColor: 'red',
+    feedbackURL: 'http://goo.gl/forms/f30sfheh4H',
+    helpURL: '/help/using-the-data-browser/',
+    showBadgeCounts: false,
+    recordUiGridEnabled: false,
+    recordUiGridExportCSVEnabled: true,
+    recordUiGridExportPDFEnabled: true,
+    editRecord: true,
+    showDeleteButton: true,
+    tour: {
+      pickRandom: false,
+      searchInputAttribute: "Data",
+      searchChosenAttribute: "Data Type",
+      searchInputValue: "micro",
+      extraAttribute: "Mouse Anatomic Source",
+      chosenAttribute: "Data Type",
+      chosenValue: "Expression microarray - gene"
+   },
+   navbarMenu: {
+       newTab: true,
+       children: [
+           {
+               name: "Search",
+               url: "/chaise/search/#1/legacy:dataset",
+               newTab: false
+           },
+           {
+               name: "RecordEdit",
+               children: [
+                   {
+                       name: "Add Records",
+                       children: [
+                           {
+                               name: "Edit Existing Record",
+                               url: "/chaise/recordedit/#1/legacy:dataset/id=5776",
+                               newTab: true
+                           }
+                       ]
+                   }
+               ],
+               newTab: true
+           }
+       ]
+   }
+};
+
+if (typeof module === 'object' && module.exports && typeof require === 'function') {
+    exports.config = chaiseConfig;
+}
