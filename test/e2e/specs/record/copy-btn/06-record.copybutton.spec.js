c415e0ba3ede96ff4772e929ba1c7b23a20b1cbc
howdyjessie <jessie@thisisjessie.com>
2016-12-23 15:13:06 -0800

    Convert sleeps to waits in record tests #853

--- test/e2e/specs/record/copy-btn/06-record.copybutton.spec.js
+++ test/e2e/specs/record/copy-btn/06-record.copybutton.spec.js
@@ -9,14 +9,17 @@ describe('View existing record,', function() {
 
         var table, record;
 
-        beforeAll(function () {
+        beforeAll(function(done) {
             var keys = [];
             tupleParams.keys.forEach(function(key) {
                 keys.push(key.name + key.operator + key.value);
             });
             browser.ignoreSynchronization=true;
-            browser.get(browser.params.url + ":" + tupleParams.table_name + "/" + keys.join("&"));
-            browser.sleep(browser.params.defaultTimeout);
+            var url = browser.params.url + ":" + tupleParams.table_name + "/" + keys.join("&");
+            browser.get(url);
+            chaisePage.waitForUrl(url).then(function() {
+                done();
+            });
         });
 
         it("should load chaise-config.js and have editRecord=true", function() {
@@ -38,37 +41,37 @@ describe('View existing record,', function() {
 
             it("should redirect to recordedit when clicked.", function() {
                 copyButton.click().then(function() {
-                    browser.sleep(100);
+                    return chaisePage.waitForUrl('recordedit');
+                }).then(function() {
                     return browser.driver.getCurrentUrl();
                 }).then(function(url) {
-                    expect(url.indexOf("recordedit")).toBeGreaterThan(-1);
-
+                    expect(url.indexOf('recordedit')).toBeGreaterThan(-1);
+                }).then(function() {
                     return chaisePage.recordEditPage.getEntityTitle();
                 }).then(function(txt) {
-                    expect(txt.indexOf("Create")).toBeGreaterThan(-1);
+                    expect(txt.indexOf('Create')).toBeGreaterThan(-1);
+                }).catch(function(error) {
+                    console.log(error);
+                    expect('There was an error.').toBe('Please check the error message.');
                 });
             });
 
             it("should alert the user if trying to submit data without changing the id.", function() {
                 chaisePage.recordEditPage.submitForm();
-
-                browser.sleep(100);
-                chaisePage.recordEditPage.getAlertError().then(function(err) {
-                    return err.getText();
-                }).then(function(text){
+                browser.wait(function() {
+                    return chaisePage.recordEditPage.getAlertError();
+                }, browser.params.defaultTimeout).then(function(error) {
+                    return error.getText();
+                }).then(function(text) {
                     expect(text.indexOf("409 Conflict")).toBeGreaterThan(-1);
                 });
             });
 
             it("should redirect to record after changing the id and resubmitting.", function() {
                 var idInput = chaisePage.recordEditPage.getInputById(0, "id");
-
-                chaisePage.recordEditPage.clearInput(idInput);
-                browser.sleep(10);
+                idInput.clear();
                 idInput.sendKeys("777");
-
                 chaisePage.recordEditPage.submitForm();
-
                 browser.driver.getCurrentUrl().then(function(url) {
                     expect(url.indexOf("record")).toBeGreaterThan(-1);
                 });
