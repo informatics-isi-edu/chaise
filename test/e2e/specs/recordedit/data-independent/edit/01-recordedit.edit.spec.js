07225dbd8a44eef8bdfc7e134c1bf6cc90273464
howdyjessie <jessie@thisisjessie.com>
2017-01-11 17:49:36 -0800

    Remove old tests

--- test/e2e/specs/recordedit/data-independent/edit/01-recordedit.edit.spec.js
+++ test/e2e/specs/recordedit/data-independent/edit/01-recordedit.edit.spec.js
@@ -43,24 +43,16 @@ describe('Edit existing record,', function() {
 				});
 
 				describe("Submitting an existing record,", function() {
-                    var updatedRecords = {}, keys = [], hasErrors = false;
+                    var keys = [], hasErrors = false;
 
 					beforeAll(function() {
-                        // Build the keys component of a url for checking:
-                        // - This row in DB
-                        // - Whether record app is on the right url
+                        // Build the keys component of a url for checking whether record app is on the right url
                         tableParams.keys.forEach(function(key) {
                             keys.push(key.name + key.operator + key.value);
                         });
 
 						// Submit the form
 						chaisePage.recordEditPage.submitForm();
-
-                        // Get the set of info you're about to submit.
-                        // Later we'll check whether this info is accurately reflected in the DB/Record app.
-                        chaisePage.recordEditPage.getSubmissionModelRows().then(function(records) {
-                            updatedRecords = records;
-                        });
 					});
 
 					it("should have no errors", function() {
@@ -73,21 +65,6 @@ describe('Edit existing record,', function() {
 							}
 						});
 					});
-
-                    it('should have updated the DB with the submitted values', function() {
-                        var url = process.env.ERMREST_URL + '/catalog/' + browser.params.catalogId + '/entity/' + browser.params.schema.name + ':' + tableParams.table_name + '/' + keys.join('&'),
-                            authCookie = process.env.AUTH_COOKIE,
-                            ermRest = require('../../../../../../../ermrestjs/build/ermrest.js');
-                        ermRest.setUserCookie(authCookie);
-                        ermRest.resolve(url, {cid: 'chaise-e2e-test'}).then(function(ref) {
-                            return ref.contextualize.entryEdit.read(1);
-                        }).then(function(page) {
-                            // Sort and compare what was submitted in RecEdit with what's in ERMrest
-                            updatedRecords.forEach(function(record, index) {
-                                expect(angular.equals(record, page.tuples[index]._data)).toBe(true);
-                            });
-                        });
-                    });
 
 					it("should redirect to Record page", function(done) {
 						if (!hasErrors) {
@@ -109,33 +86,4 @@ describe('Edit existing record,', function() {
 
     	})(testParams.tables[i], i);
     }
-    xdescribe('submitting the form without making any changes', function() {
-        var keys = [], tableParams = testParams.tables[0];
-        beforeAll(function() {
-            tableParams.keys.forEach(function(key) {
-                keys.push(key.name + key.operator + key.value);
-            });
-            browser.ignoreSynchronization=true;
-            browser.get(browser.params.url + ":" + tableParams.table_name + "/" + keys.join("&"));
-
-            chaisePage.waitForElement(element(by.id("submit-record-button")), browser.params.defaultTimeout).then(function() {
-            	chaisePage.recordEditPage.submitForm();
-            });
-        });
-
-        it('should also redirect to the correct Record page', function() {
-        	var recordPage = chaisePage.recordPage,
-                redirectUrl = browser.params.url.replace('/recordedit/', '/record/');
-
-            redirectUrl += ':' + tableParams.table_name + '/' + keys.join('&');
-            browser.pause();
-
-        	chaisePage.waitForUrl(redirectUrl, browser.params.defaultTimeout).then(function() {
-                expect(browser.driver.getCurrentUrl()).toBe(redirectUrl);
-            }, function() {
-            	console.log("          Timed out while waiting for the url to be the new one");
-            	expect(browser.driver.getCurrentUrl()).toBe(redirectUrl);
-            });
-        });
-    });
 });
