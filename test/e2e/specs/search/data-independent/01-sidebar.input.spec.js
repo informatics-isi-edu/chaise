094526873bb4d983285dd205f10dbdf555717030
Joshua Chudy <jchudy@isi.edu>
2017-01-27 17:36:39 -0800

    commit before context switch

--- test/e2e/specs/search/data-independent/01-sidebar.input.spec.js
+++ test/e2e/specs/search/data-independent/01-sidebar.input.spec.js
@@ -11,16 +11,17 @@ describe('Sidebar top search input,', function () {
         browser.get(browser.params.url || "");
         var sidebar = element(by.id('sidebar'));
         browser.wait(EC.visibilityOf(sidebar), browser.params.defaultTimeout);
-
     });
+
     var searchBox = chaisePage.sidebar.searchInput;
     var displayedAttrs = chaisePage.sidebar.sidebarAttrsDisplayed;
 
     var initAttrNum;
-    it('should show >0 attributes when in initial state', function () {
+    it('should show >0 attributes when in initial state', function (done) {
         displayedAttrs.count().then(function (num) {
             initAttrNum = num;
             expect(num).toBeGreaterThan(0);
+            done();
         });
     });
 
@@ -28,10 +29,8 @@ describe('Sidebar top search input,', function () {
     it('should input meaningless text and wait for seconds', function () {
         searchBox.sendKeys(meaninglessTxt);
         searchBox.sendKeys(protractor.Key.BACK_SPACE);
-        setTimeout(function () {}, 4000);
-    });
-
-    it('should show 0 attribute when searching for meaningless input', function () {
+        browser.sleep(100);
+        browser.pause();
         expect(displayedAttrs.count()).toBe(0);
     });
 
