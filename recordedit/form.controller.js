33d140903b4443fc3c44ef295b96cb79408a601b
howdyjessie <jessie@isi.edu>
2017-03-12 17:49:11 -0700

    Do not apply view model transformations to columns that are disabled #1016

--- recordedit/form.controller.js
+++ recordedit/form.controller.js
@@ -117,6 +117,13 @@
              */
             for (var i = 0; i < $rootScope.reference.columns.length; i++) {
                 var col = $rootScope.reference.columns[i];
+
+                // If this column is disabled, its value is already in the format
+                // needed for submission.
+                if (col.getInputDisabled(context.appContext)) {
+                    continue;
+                }
+
                 var rowVal = row[col.name];
                 if (rowVal) {
                     switch (col.type.name) {
@@ -160,7 +167,6 @@
             vm.readyToSubmit = true;
             vm.submissionButtonDisabled = true;
             for (var j = 0; j < model.rows.length; j++) {
-                var transformedRow = transformRowValues(model.rows[j]);
                 $rootScope.reference.columns.forEach(function (column) {
                     // If the column is a pseudo column, it needs to get the originating columns name for data submission
                     if (column.isPseudo) {
@@ -188,6 +194,7 @@
                         }
                     // not pseudo, column.name is sufficient for the keys
                     } else {
+                        var transformedRow = transformRowValues(model.rows[j]);
                         // set null if not set so that the whole data object is filled out for posting to ermrestJS
                         model.submissionRows[j][column.name] = (transformedRow[column.name] === undefined) ? null : transformedRow[column.name];
                     }
@@ -207,7 +214,6 @@
                 // submit $rootScope.tuples because we are changing and comparing data from the old data set for the tuple with the updated data set from the UI
                 $rootScope.reference.update($rootScope.tuples).then(function success(page) {
                     if (window.opener && window.opener.updated) {
-                        console.dir('I ran here and here is my opener:', window.opener);
                         window.opener.updated(context.queryParams.invalidate);
                     }
                     vm.readyToSubmit = false; // form data has already been submitted to ERMrest
--- recordedit/recordEdit.app.js
+++ recordedit/recordEdit.app.js
@@ -176,12 +176,20 @@
                                 for (var i = 0; i < $rootScope.reference.columns.length; i++) {
                                     column = $rootScope.reference.columns[i];
 
+                                    // If input is disabled, a disabled text input field is
+                                    // used in the UI. As of writing, ermrestjs
+                                    // formats column values as strings so there's
+                                    // no need to transform the column value.
+                                    if (column.getInputDisabled(context.appContext)) {
+                                        recordEditModel.rows[j][column.name] = values[i];
+                                        continue;
+                                    }
+
+                                    // Transform column values for use in view model
                                     switch (column.type.name) {
                                         case "timestamp":
                                         case "timestamptz":
                                             if (values[i]) {
-                                                // Cannot ensure that all timestamp values are formatted in ISO 8601
-                                                // TODO: Fix pretty print fn in ermrestjs to return ISO 8601 format instead of toLocaleString?
                                                 var ts = moment(values[i]);
                                                 value = {
                                                     date: ts.format('YYYY-MM-DD'),
