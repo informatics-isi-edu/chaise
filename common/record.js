7508f61529e0dbd9d616d11f0fe2241d1934dbdc
Aref Shafaei <aref.shafaei@gmail.com>
2017-06-07 11:29:00 -0700

    change angular UI's bind-html-unsafe to angular ng-bind-html
    We are currently using old version of angular UI. The current version
    has deprecated this api. Besides bind-html-unsafe ignores texts that
    don't have any HTML tags. There are some cases that eventhough we trust
    the value to be HTML, there might not be any HTML tags in it. For
    instance if the row_markdown_pattern is just a simple string without any
    HTML tags.

--- common/record.js
+++ common/record.js
@@ -3,6 +3,12 @@
 
     angular.module('chaise.record.display', [])
 
+    .filter('trustedHTML', ['$sce', function($sce){
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    }])
+
     .directive('recordDisplay', [function() {
         return {
             restrict: 'E',
