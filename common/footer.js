8230e772dd4e6f06064535aa5324075dfcacb507
amitjha21 <amitjha@usc.edu>
2017-06-27 18:19:17 -0700

    directive update

--- common/footer.js
+++ common/footer.js
@@ -1,59 +1,51 @@
 /**
-* @desc
-* @param {String} textinput The Markdown to transform
-* @example <a href class="live-preview"  markdown-preview textinput="form.recordEditModel.rows[rowIndex][column.name]">Live Preview</a>
-* @example <markdown-preview class="live-preview" textinput="form.recordEditModel.rows[rowIndex][column.name]">Click</markdown-preview>
-*/
+ * @desc
+ * @param {String} textinput The Markdown to transform
+ * @example <a href class="live-preview"  markdown-preview textinput="form.recordEditModel.rows[rowIndex][column.name]">Live Preview</a>
+ * @example <markdown-preview class="live-preview" textinput="form.recordEditModel.rows[rowIndex][column.name]">Click</markdown-preview>
+ */
 
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('chaise.footer', [])
-    .directive('footer', ['ERMrest', '$sce','$window', '$document', '$timeout',function(ERMrest, $sce, $window, $document,$timeout) {
+    angular.module('chaise.footer', [])
+        .directive('footer', ['ERMrest', '$sce', '$window', '$document', '$timeout', function(ERMrest, $sce, $window, $document, $timeout) {
 
-      return {
-        restrict: 'E',
-        scope: {},
-        templateUrl: '../common/templates/footer.html',
-        link: function(scope) {
-                var footerText = chaiseConfig.footerMarkdown;
-                angular.isUndefinedOrNull = function(val) {
-                  return val == '' || angular.isUndefined(val) || val === null
-                }
-                $timeout(function(){
-                if ($(document).height() > $(window).height()) { //scrolling
-                    scope.posStyle = {
-                        width: 'auto',
-                        height: '30px',
-                    	'background-color': '#f2f2f2',
-                        right: 0,
-                        bottom: '0',
-                        left: '0',
-                        position:'relative'
-                    };
-                }
-                else{
-                    scope.posStyle = {
-                        width: 'auto',
-                        height: '30px',
-                    	'background-color': '#f2f2f2',
-                        right: '0',
-                        bottom: '0',
-                        left: '0',
-                        position:'absolute'
-                    };
-                }
-                if (angular.isUndefinedOrNull(footerText))
-                  scope.privacyResult = "**Default Privacy Policy**";
-                else{
-                    ERMrest._onload().then(function(){
-                            scope.privacyResult = ERMrest.renderMarkdown(footerText);
-                    })
+            return {
+                restrict: 'E',
+                scope: {},
+                templateUrl: '../common/templates/footer.html',
+                link: function(scope) {
+                    var footerText = chaiseConfig.footerMarkdown;
+                    angular.isUndefinedOrNull = function(val) {
+                        return val == '' || angular.isUndefined(val) || val === null
+                    }
+                    $timeout(function() {
+                        scope.posStyle = {
+                            width: 'auto',
+                            height: '30px',
+                            'background-color': '#f2f2f2',
+                            right: 0,
+                            bottom: 0,
+                            left: 0
+                        };
 
-            }
-            // });
-        },1000);
-        }
-      };
-    }])
+                        if ($(document).height() > $(window).height()) { //scrolling
+                            scope.posStyle.position = 'relative';
+                        } else {
+                            scope.posStyle.position = 'absolute';
+                        }
+                        if (angular.isUndefinedOrNull(footerText))
+                            scope.privacyResult = "**Default Privacy Policy**";
+                        else {
+                            ERMrest._onload().then(function() {
+                                scope.privacyResult = ERMrest.renderMarkdown(footerText);
+                            })
+
+                        }
+                        // });
+                    }, 1500);
+                }
+            };
+        }])
 })();
