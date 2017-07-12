011e3e855f7b10df39fef3e28c16085467bea226
Nitish Sahu <nitish@isi.edu>
2017-07-12 14:25:02 -0700

    Changes made to allow a modal window login

--- common/errors.js
+++ common/errors.js
@@ -85,7 +85,11 @@
                     
             modalInstance.result.then(function () {
                 if (errorCode == errorNames.unauthorized && !providedLink) {
-                    Session.loginInAPopUp(reloadCb);                
+                    var x = window.innerWidth/2 - 800/2;
+                    var y = window.innerHeight/2 - 600/2;
+
+                    var win = window.open("", '_blank','width=800,height=600,left=' + x + ',top=' + y);
+                    Session.loginInAPopUp(win, reloadCb);                
                 } else {
                     $window.location.replace(redirectLink);
                 }
@@ -131,7 +135,7 @@
             if (exceptionFlag || window.location.pathname.indexOf('/search/') != -1 || window.location.pathname.indexOf('/viewer/') != -1) return;
 
             if (ERMrest && exception instanceof ERMrest.UnauthorizedError || exception.code == errorNames.unauthorized) {
-                Session.loginInAPopUp(reloadCb);
+                Session.loginInAModal(reloadCb);
             } else {
                 var errName = exception.constructor.name;
                 errName = (errName.toLowerCase() !== 'error') ? errName : "Terminal Error";
