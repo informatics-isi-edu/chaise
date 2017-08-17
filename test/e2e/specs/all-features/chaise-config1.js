783ae60e1b842cc14069c9be5ef0f5a412a70770
Nitish Sahu <nitish@isi.edu>
2017-08-16 17:20:10 -0700

    New chaise config to be used by specs to test collapsable tables

---
+++ test/e2e/specs/all-features/chaise-config1.js
@@ -0,0 +1,41 @@
+// Configure deployment-specific data here
+
+var chaiseConfig = {
+    name: "All Features",
+    editRecord: true,
+    deleteRecord: true,
+    // configuration for navbar spec with base condig
+    navbarBrand: 'test123',
+    navbarBrandImage: '../images/genetic-data.png',
+    navbarBrandText: 'test123',
+    headTitle: 'this one should be ignored in favor of navbarBrandText',
+    // config for biewer spec
+    customCSS: '/path/to/custom/css',
+    maxRelatedTablesOpen: 4,
+    navbarMenu: {
+       newTab: true,
+       children: [
+           {
+               name: "Search",
+               url: "/chaise/search/#1/legacy:dataset",
+               newTab: false
+           },
+           {
+               name: "RecordEdit",
+               children: [
+                   {
+                       name: "Add Records",
+                       children: [
+                           {
+                               name: "Edit Existing Record",
+                               url: "/chaise/recordedit/#1/legacy:dataset/id=5776",
+                               newTab: true
+                           }
+                       ]
+                   }
+               ],
+               newTab: true
+           }
+       ]
+   }
+};
