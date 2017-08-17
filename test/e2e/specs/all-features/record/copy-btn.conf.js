0228098e56cd176c048fa856c9386a60deb4ea86
Nitish Sahu <nitish@isi.edu>
2017-08-16 18:18:08 -0700

    chaise config now points to only available config file

--- test/e2e/specs/all-features/record/copy-btn.conf.js
+++ test/e2e/specs/all-features/record/copy-btn.conf.js
@@ -2,7 +2,7 @@ var pConfig = require('./../../../utils/protractor.configuration.js');
 
 var config = pConfig.getConfig({
     configFileName: 'record/copy-btn.dev.json',
-    chaiseConfigFilePath: 'test/e2e/specs/all-features/chaise-config1.js',
+    chaiseConfigFilePath: 'test/e2e/specs/all-features/chaise-config.js',
     specs: [
         "copy-btn.spec.js"
     ],
