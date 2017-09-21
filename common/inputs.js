6cb88368fb5343a1e2ccda15c093ab685705d44b
Joshua Chudy <jchudy@isi.edu>
2017-09-20 17:31:31 -0700

    validators for time/date.numeric range inputs

--- common/inputs.js
+++ common/inputs.js
@@ -49,18 +49,60 @@
                     return relativeType;
                 }
 
+                // initialize properties
+                if (scope.displayType(scope.type) == "datetime") {
+                    scope.min = {
+                        date: null,
+                        time: null
+                    };
+                    scope.max = {
+                        date: null,
+                        time: null
+                    };
+                } else {
+                    scope.min = null;
+                    scope.max = null;
+                }
+                
+                scope.numericError = function () {
+                    return scope.numericMin.$error || scope.numericMax.$error;
+                }
+
+                scope.showNumericError = function () {
+                    return (scope.numericMin.$dirty && scope.numericMin.$error.integer) || (scope.numericMax.$dirty && scope.numericMax.$error.integer) || scope.minMaxForm.$submitted;
+                }
+
                 // returns a boolean to disable the add button if both min and max are not set
+                // for timestamps/datetime, we don't care if the time is not set
                 scope.disableAdd = function () {
-                    return ( emptyOrNull(scope.min) && emptyOrNull(scope.max) )
+                    return (scope.displayType(scope.type) == "datetime") ? ( emptyOrNull(scope.min.date) && emptyOrNull(scope.max.date) ) : ( emptyOrNull(scope.min) && emptyOrNull(scope.max) );
                 };
+
+                scope.dateError = function () {
+                    return scope.dateMin.$error || scope.dateMax.$error || scope.tsDateMin.$error || scope.tsDateMax.$error;
+                }
+                
+                //TODO refactor for each form
+                scope.showDateError = function () {
+                    return (scope.dateMin.$dirty && scope.dateMin.$error.date) || (scope.dateMax.$dirty && scope.dateMax.$error.date) || (scope.tsDateMin.$dirty && scope.tsDateMin.$error.date) || (scope.tsDateMax.$dirty && scope.tsDateMax.$error.date) || scope.minMaxForm.$submitted;
+                }
+
+                scope.timeError = function () {
+                    return scope.timeMin.$error || scope.timeMax.$error;
+                }
+
+                scope.showTimeError = function () {
+                    return (scope.timeMin.$dirty && scope.timeMin.$error.time) || (scope.timeMax.$dirty && scope.timeMax.$error.time) || scope.minMaxForm.$submitted;
+                }
 
                 scope.addRange = function () {
                     var min, max;
                     scope.isDirty = true;
                     // data for timestamp[tz] needs to be formatted properly
                     if (scope.displayType(scope.type) == "datetime") {
-                        min = moment(scope.min.date + scope.min.time, 'YYYY-MM-DDHH:mm:ss').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
-                        max = moment(scope.max.date + scope.max.time, 'YYYY-MM-DDHH:mm:ss').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+                        console.log(scope.min.date + scope.min.time);
+                        min = (scope.min.date) ? moment(scope.min.date + scope.min.time, 'YYYY-MM-DDHH:mm:ss').format('YYYY-MM-DDTHH:mm:ss.SSSZ') : '';
+                        max = (scope.max.date) ? moment(scope.max.date + scope.max.time, 'YYYY-MM-DDHH:mm:ss').format('YYYY-MM-DDTHH:mm:ss.SSSZ') : '';
                     } else {
                         min = scope.min;
                         max = scope.max;
