e12b5fffebf2c5f6194d74df4cac37523acfc23a
Joshua Chudy <jchudy@isi.edu>
2017-10-11 12:36:05 -0700

    basic structure for faceting specs

--- test/e2e/specs/delete-prohibited/recordset/ind-facet.spec.js
+++ test/e2e/specs/delete-prohibited/recordset/ind-facet.spec.js
@@ -1,54 +1,71 @@
 var chaisePage = require('../../../utils/chaise.page.js');
 var testParams = {
     table_name: "main",
-    openFacetNames: [ 'id', 'int_col', 'to_name' ]
+    openFacetNames: [ "id", "int_col", "to_name" ]
 }
 
-describe('Delete existing record,', function() {
+describe("Viewing Recordset with Faceting,", function() {
 
     describe("For table " + testParams.table_name + ",", function() {
 
-        var table, record;
+        var table, record,
+            uri = browser.params.url + "/recordset/#" + browser.params.catalogId + "/faceting:" + testParams.table_name;
 
         beforeAll(function () {
             browser.ignoreSynchronization=true;
-            browser.get(browser.params.url + "/recordset/#" + browser.params.catalogId + "/faceting:" + testParams.table_name);
+            browser.get(uri);
             chaisePage.waitForElementInverse(element(by.id("spinner")));
         });
-
-        it("should have 3 facets open", function () {
-            chaisePage.recordsetPage.getOpenFacets().count().then(function (ct) {
-                expect(ct).toBe(3, "Number of open facets is incorrect");
-
-                return chaisePage.recordsetPage.getOpenFacetTitles();
-            }).then(function (text) {
-                expect(text).toEqual(testParams.openFacetNames, "Names of open facets are incorrect");
+        
+        describe("default presentation based on facets annotation ", function () {
+            it("should have 3 facets open", function () {
+                chaisePage.recordsetPage.getOpenFacets().count().then(function (ct) {
+                    expect(ct).toBe(3, "Number of open facets is incorrect");
+                    
+                    return chaisePage.recordsetPage.getOpenFacetTitles();
+                }).then(function (text) {
+                    expect(text).toEqual(testParams.openFacetNames, "Names of open facets are incorrect");
+                });
+            });
+            
+            it("should have 2 filters selected", function () {
+                chaisePage.recordsetPage.getFilters().count().then(function (ct) {  
+                    expect(ct).toBe(2, "Number of visible filters is incorrect");
+                });
+            });
+            
+            it("should have 1 row visible", function () {
+                chaisePage.recordsetPage.getRows().count().then(function (ct) {
+                    expect(ct).toBe(1, "Number of visible rows is incorrect");
+                });
+            });
+            
+            it("should show 25 rows and 0 filters after clicking 'clear all'", function () {
+                var sortString = "@sort(id)";
+                chaisePage.recordsetPage.getClearAllFilters().click().then(function () {
+                    return chaisePage.waitForElementInverse(element(by.id("spinner")));
+                }).then(function () {
+                    //verify there's no facet string in url
+                    return browser.getCurrentUrl();
+                }).then(function (url) {
+                    expect(url).toBe(uri + sortString);
+                    
+                    return chaisePage.recordsetPage.getRows().count();
+                }).then(function (ct) {
+                    expect(ct).toBe(25, "Number of visible rows is incorrect");
+                    
+                    return chaisePage.recordsetPage.getFilters().count();
+                }).then(function (ct) {
+                    expect(ct).toBe(0, "Number of visible filters is incorrect");
+                });
             });
         });
 
-        it("should have 2 filters selected", function () {
-            chaisePage.recordsetPage.getFilters().count().then(function (ct) {  
-                expect(ct).toBe(2, "Number of visible filters is incorrect");
-            });
-        });
+        describe("selecting individual filters for each facet type", function () {
+            
 
-        it("should have 1 row visible", function () {
-            chaisePage.recordsetPage.getRows().count().then(function (ct) {
-                expect(ct).toBe(1, "Number of visible rows is incorrect");
-            });
-        });
-
-        it("should show 25 rows and 0 filters after clicking 'clear all'", function () {
-            chaisePage.recordsetPage.getClearAllFilters().click().then(function () {
-                return chaisePage.waitForElementInverse(element(by.id("spinner")));
-            }).then(function () {
-                return chaisePage.recordsetPage.getRows().count();
-            }).then(function (ct) {
-                expect(ct).toBe(25, "Number of visible rows is incorrect");
-
-                return chaisePage.recordsetPage.getFilters().count();
-            }).then(function (ct) {
-                expect(ct).toBe(0, "Number of visible filters is incorrect");
+            it("should open the facet, select a value, and verify the data is filtered.", function () {
+                browser.pause();
             });
         });
     });
