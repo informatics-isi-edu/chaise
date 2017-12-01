59546cf0085f3c9c8399f74205180cc593685f90
Joshua Chudy <jchudy@isi.edu>
2017-11-30 17:47:17 -0800

    undo changes to run all tests

--- test/e2e/specs/default-config/protractor.conf.js
+++ test/e2e/specs/default-config/protractor.conf.js
@@ -4,7 +4,7 @@ var config = pConfig.getConfig({
     // This config is meant to be run as part of the parallel tests configuration
     configFileName: 'parallel-configs/default-config.dev.json',
     specs: [
-        "recordedit/immutable-inputs.spec.js"
+        "*/*.spec.js"
     ],
     setBaseUrl: function(browser, data) {
         browser.params.url = process.env.CHAISE_BASE_URL;
