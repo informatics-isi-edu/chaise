ad628d6ece6a9e97bf095376b3459e8b950f29b1
Joshua Chudy <jchudy@isi.edu>
2017-12-01 16:58:46 -0800

    undo some commented lines to make build faster for verifying failing test

--- test/e2e/specs/delete-prohibited/protractor.conf.js
+++ test/e2e/specs/delete-prohibited/protractor.conf.js
@@ -4,7 +4,7 @@ var config = pConfig.getConfig({
     // This config is meant to be run as part of the parallel tests configuration
     configFileName: 'parallel-configs/delete-prohibited.dev.json',
     specs: [
-        "recordset/ind-facet.spec.js"
+        "*/*.spec.js"
     ],
     setBaseUrl: function(browser, data) {
         browser.params.url = process.env.CHAISE_BASE_URL;
