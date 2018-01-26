ad5aae102205a8caa95780307fbc58aa4e289c59
Aref Shafaei <aref.shafaei@gmail.com>
2018-01-26 12:59:10 -0800

    ignore search and viewer test cases

--- test/e2e/specs/all-features/viewer/presentation.conf.js
+++ test/e2e/specs/all-features/viewer/presentation.conf.js
@@ -3,6 +3,9 @@ var pConfig = require('./../../../utils/protractor.configuration.js');
 var config = pConfig.getConfig({
     configFileName: 'viewer/dev.json',
     chaiseConfigFilePath: 'test/e2e/specs/all-features/chaise-config.js',
+    specs: [
+        "presentation.spec.ignored.js"
+    ],
     setBaseUrl: function(browser, data) {
         browser.params.url = process.env.CHAISE_BASE_URL;
         return browser.params.url;
