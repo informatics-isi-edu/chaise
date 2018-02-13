8e74e2c6fbab923c21322c3a8129688fd17c20d1
Joshua Chudy <jchudy@isi.edu>
2018-02-13 11:57:33 -0800

    display uri too

--- common/faceting.js
+++ common/faceting.js
@@ -706,7 +706,7 @@
 
                                 if (isColumnOfType("timestamp")) {
                                     AlertsService.addAlert("1. data before padding " + response.xPadding, 'error');
-                                    AlertsService.addAlert("2. data from API " + response.x, 'success');
+                                    AlertsService.addAlert("2. uri " + response.uri, 'success');
                                 }
 
                                 scope.plot.labels = response.labels;
