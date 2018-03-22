652a668756da42be342bef09dcc595762e0d152d
Joshua Chudy <jchudy@isi.edu>
2018-03-22 13:06:32 -0700

    fixed unauthorized message when not logged in to be more user friendlu

--- common/modal.js
+++ common/modal.js
@@ -45,26 +45,28 @@
         vm.showReloadBtn = false;
         var reloadMessage = ' <p>  </p>';
 
-        if(vm.params.errorCode == 'Multiple Records Found'){
+        if (vm.params.errorCode == 'Multiple Records Found') {
             vm.clickActionMessage =  messageMap.recordAvailabilityError.multipleRecords;
-        } else if(vm.params.errorCode == 'Record Not Found'){
+        } else if (vm.params.errorCode == 'Record Not Found') {
             vm.clickActionMessage = messageMap.recordAvailabilityError.noRecordsFound;
-            if(params && !params.isLoggedIn){
-              params.message = messageMap.noRecordForFilter + '<br>' + messageMap.unauthorizedMessage;
+
+            // if no user logged in, change message
+            if (params && !params.isLoggedIn) {
+                params.message = messageMap.noRecordForFilter + '<br>' + messageMap.maybeUnauthorizedMessage;
             }
         } else if (Object.values(messageMap.facetRelatedErrorStatus).indexOf(vm.params.errorCode) > -1) {
-           // Check if error prompted was found in the facetRelatedErrorStatus object and use it to
-           // generate error phrase for action message
-            if(vm.params.errorCode == messageMap.facetRelatedErrorStatus.invalidFilter){
+            // Check if error prompted was found in the facetRelatedErrorStatus object and use it to
+            // generate error phrase for action message
+            if (vm.params.errorCode == messageMap.facetRelatedErrorStatus.invalidFilter) {
                 vm.clickActionMessage = messageMap.recordAvailabilityError.noRecordsFound;
-            } else{
-            vm.clickActionMessage = messageMap.facetRelatedErrorStatus.clickActionMessage.replace('@errorStatus', vm.params.errorCode);
-          }
-        }else {
+            } else {
+                vm.clickActionMessage = messageMap.facetRelatedErrorStatus.clickActionMessage.replace('@errorStatus', vm.params.errorCode);
+            }
+        } else {
             vm.clickActionMessage = messageMap.recordAvailabilityError.pageRedirect + vm.params.pageName + '. ';
-            if(vm.params.appName == 'recordedit'){
-              vm.showReloadBtn = true;
-              reloadMessage = ' <p>' + messageMap.terminalError.reloadMessage +' </p>';
+            if (vm.params.appName == 'recordedit'){
+                vm.showReloadBtn = true;
+                reloadMessage = ' <p>' + messageMap.terminalError.reloadMessage +' </p>';
             }
         }
         // <p> tag is added to maintain the space between click action message and buttons
@@ -83,13 +85,13 @@
         vm.cancel = function cancel() {
             $uibModalInstance.dismiss('cancel');
         };
+
         vm.reload = function () {
             $uibModalInstance.close("reload");
-
         };
+
         vm.login = function () {
             Session.loginInAPopUp();  //Open login pop-up without closing error modal
-
         };
 
 
