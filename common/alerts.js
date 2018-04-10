643ce06fa2c96c37cbba9435ed65682f1fd5679a
Joshua Chudy <jchudy@isi.edu>
2018-04-10 14:56:08 -0700

    fixed alert CSS/styling. added comment about why fextra functions will be defined in alert controller

--- common/alerts.js
+++ common/alerts.js
@@ -51,6 +51,9 @@
                     AlertsService.deleteAlert(alert);
                 };
 
+                /****** Functions used within alert messages *******/
+                // defined here to allow for any callback to be used with the alert messages
+                // passing along the named functions and attaching them to the alert scope may be over-engineering, so this will suffice for now
                 scope.login = function() {
                     Session.loginInAPopUp();
                 };
