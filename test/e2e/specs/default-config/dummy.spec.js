fe0115ce2a92fb6504db5d1d520e2a2fbe0ba1ed
Aref Shafaei <aref.shafaei@gmail.com>
2018-07-18 11:48:45 -0700

    add dummy specs

---
+++ test/e2e/specs/default-config/dummy.spec.js
@@ -0,0 +1,5 @@
+describe('dummy spec 3', function() {
+    it ("dummy it", function () {
+        expect(true).toBe(true);
+    });
+});
---
+++ test/e2e/specs/delete-prohibited/dummy.spec.js
@@ -0,0 +1,5 @@
+describe('dummy spec 4', function() {
+    it ("dummy it", function () {
+        expect(true).toBe(true);
+    });
+});
