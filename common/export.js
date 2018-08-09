6456b2289695b1ad181e4e9c9a9b7e0f4f7f7b23
Aref Shafaei <aref.shafaei@gmail.com>
2018-08-09 12:16:50 -0700

    export changes: cancel button + progress modal

--- common/export.js
+++ common/export.js
@@ -3,22 +3,103 @@
 
     angular.module('chaise.export', [])
 
-    .constant('formatOptions', {
-        "BAG": {
-            algs: ["md5"],
-            archiver: "zip",
-            metadata: {},
-            table_format: "csv"
-        }
-    })
+    .directive('export', ['AlertsService', 'logActions', 'modalUtils', '$timeout', function (AlertsService, logActions, modalUtils, $timeout) {
 
-    .directive('export', ['AlertsService', 'formatOptions', function (AlertsService, formatOptions) {
+        /**
+         * Cancel the current export request
+         */
+        function _cancelExport(scope) {
+            if (scope.exporter) {
+                scope.exporter.cancel();
+            }
+            scope.isLoading = false;
+            AlertsService.addAlert("Export request has been canceled.", "warning");
+        }
+
+        /**
+         * Update the list of templates in UI
+         */
+        function _updateExportFormats(scope) {
+            var templates = scope.reference.table.exportTemplates;
+
+            templates.forEach(function (template) {
+                if (template.format_name) {
+                    // matches object format set for default case with CSV
+                    scope.exportOptions.supportedFormats.push(template);
+                }
+            });
+        }
+
+        /**
+         * Send the request for export
+         */
+        function _doExport(scope, template) {
+            var formatName = template.format_name;
+            var formatType = template.format_type;
+            switch (formatType) {
+                case "DIRECT":
+                    if (formatName === "CSV") {
+                        location.href = scope.reference.csvDownloadLink;
+                    }
+                    // NOTE: uncomment below when we want to support JSON
+                    // else if (exportFormatName === "JSON") {
+                    //     location.href = scope.reference.jsonDownloadLink;
+                    // }
+                    break;
+                case "BAG":
+                case "FILE":
+                    scope.exporter = new ERMrest.Exporter(scope.reference, template);
+                    var exportParametersString = JSON.stringify(scope.exporter.exportParameters, null, "  ");
+
+                    // begin export and start a timer
+                    console.info("Executing external export with the following parameters:\n" + exportParametersString);
+                    console.time('External export duration');
+                    scope.progressModal = modalUtils.showModal({
+                        templateUrl: "../common/templates/exportProgress.modal.html",
+                        controller: "ExportProgressController",
+                        windowClass: "export-progress",
+                        controllerAs: "ctrl",
+                        size: "md",
+                        backdrop: 'static',
+                        keyboard: false,
+                        resolve: {
+                            params: {
+                                format_name: template.format_name
+                            }
+                        }
+                    }, null, function () {
+                        _cancelExport(scope);
+                    });
+                    scope.isLoading = true;
+
+                    scope.exporter.run({action: logActions.export}).then(function (response) {
+                        // if it was canceled, just ignore the result
+                        if (response.canceled) return;
+
+                        console.timeEnd('External export duration');
+                        scope.progressModal.close("Done");
+                        scope.isLoading = false;
+
+                        location.href = response.data[0];
+                    }).catch(function (error) {
+                        console.timeEnd('External export duration');
+                        scope.progressModal.close("Done");
+                        scope.isLoading = false;
+                        AlertsService.addAlert("Export failed. Please report this problem to your system administrators. Details: " + error.message, "error");
+                    });
+                    break;
+                default:
+                    AlertsService.addAlert("Unsupported export format: " + formatType + ". Please report this problem to your system administrators.");
+            }
+        }
+
         return {
             restrict: 'AE',
             templateUrl: '../common/templates/export.html',
             scope: {
                 reference: "=",
-                hasValues: "="
+                hasValues: "=",
+                disabled: "="
             },
             link: function (scope, element, attributes) {
                 scope.isLoading = false;
@@ -36,68 +117,16 @@
                 };
 
                 scope.submit = function (template) {
-                    scope.isLoading = true;
                     // The actual export code - it invokes a (synchronous) web service call to either
                     // ermrest (for single table CSV or JSON export) or ioboxd (if bag or multi-file export)
-                    _doExport(template);
+                    _doExport(scope, template);
                 };
 
                 scope.$watch('hasValues', function (newValue, oldValue) {
                     if (newValue) {
-                        formatOptions.BAG.name = scope.reference.location.tableName;
-
-                        _updateExportFormats();
+                        _updateExportFormats(scope);
                     }
                 });
-
-                function _updateExportFormats() {
-                    var templates = scope.reference.table.exportTemplates
-
-                    templates.forEach(function (template) {
-                        if (template.format_name) {
-                            // matches object format set for default case with CSV
-                            scope.exportOptions.supportedFormats.push(template);
-                        }
-                    });
-                }
-
-                function _doExport(template) {
-                    scope.exporter = new ERMrest.Exporter(scope.reference, template);
-                    var formatName = template.format_name;
-                    var formatType = template.format_type;
-                    switch (formatType) {
-                        case "DIRECT":
-                            if (formatName === "CSV") {
-                                location.href = scope.reference.csvDownloadLink;
-                            }
-                            // NOTE: uncomment below when we want to support JSON
-                            // else if (exportFormatName === "JSON") {
-                            //     location.href = scope.reference.jsonDownloadLink;
-                            // }
-                            scope.isLoading = false;
-                            break;
-                        case "BAG":
-                        case "FILE":
-                            var exportParametersString = JSON.stringify(scope.exporter.exportParameters, null, "  ");
-
-                            // begin export and start a timer
-                            console.info("Executing external export with the following parameters:\n" + exportParametersString);
-                            console.time('External export duration');
-                            scope.exporter.invokeExternalExport().then(function (response) {
-                                location.href = response[0];
-
-                                console.timeEnd('External export duration');
-                                scope.isLoading = false;
-                            }).catch(function (error) {
-                                console.timeEnd('External export duration');
-                                AlertsService.addAlert("Export failed for " + scope.exporter.exportParameters.catalog.host + "/iobox/export/ with " + error.message, "error");
-                                scope.isLoading = false;
-                            });
-                            break;
-                        default:
-                            AlertsService.addAlert("Unsupported export format: " + formatType);
-                    }
-                }
             }
         };
     }]);
