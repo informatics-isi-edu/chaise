07257dc514ef7abd65dd0b57a07639c65d4b9f4f
Joshua Chudy <jchudy@isi.edu>
2018-08-22 14:52:14 -0700

    call handleException with dismissable property for export errors

--- common/export.js
+++ common/export.js
@@ -3,7 +3,7 @@
 
     angular.module('chaise.export', ['chaise.utils'])
 
-    .directive('export', ['AlertsService', 'DataUtils', 'logActions', 'modalUtils', '$timeout', 'UriUtils', function (AlertsService, DataUtils, logActions, modalUtils, $timeout, UriUtils) {
+    .directive('export', ['AlertsService', 'DataUtils', 'ErrorService', 'logActions', 'modalUtils', '$timeout', 'UriUtils', function (AlertsService, DataUtils, ErrorService, logActions, modalUtils, $timeout, UriUtils) {
 
         /**
          * Cancel the current export request
@@ -88,12 +88,10 @@
                         error.subMessage = error.message;
                         error.message = "Export failed. Please report this problem to your system administrators.";
                         ErrorService.handleException(error, true);
-                        // AlertsService.addAlert("Export failed. Please report this problem to your system administrators. Details: " + error.message, "error");
                     });
                     break;
                 default:
                     ErrorService.handleException(new Error("Unsupported export format: " + formatType + ". Please report this problem to your system administrators."), true);
-                    // AlertsService.addAlert("Unsupported export format: " + formatType + ". Please report this problem to your system administrators.");
             }
         }
 
