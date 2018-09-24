2075a3172b740f1644ca71afe4a13f9189ccfa9f
Aref Shafaei <aref.shafaei@gmail.com>
2018-09-24 13:05:21 -0700

    fix check presence bug
    The list of filters that we're showing to the users is different from
    the actual list of filters that are used for faceting. We have done this
    because we wanted to keep track of these fitlers. For the check presence,
    instead of showing the null filter, we were always showing not-null filter.

--- common/faceting.js
+++ common/faceting.js
@@ -1426,7 +1426,7 @@
                         scope.checkboxRows[1].selected = false;
                         if (scope.facetColumn.hasNullFilter) {
                             scope.checkboxRows[1].selected = true;
-                            scope.facetModel.appliedFilters.push(facetingUtils.getNotNullFilter());
+                            scope.facetModel.appliedFilters.push(facetingUtils.getNullFilter());
                         }
 
                         return defer.resolve(true), defer.promise;
