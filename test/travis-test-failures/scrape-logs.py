0a7ed8c3f399e99ccf9dc97c9f52fd0e3b8f6140
Joshua Chudy <jchudy@isi.edu>
2019-02-26 17:17:53 -0800

    cleaned up script so it's more readable. more information included in reports

--- test/travis-test-failures/scrape-logs.py
+++ test/travis-test-failures/scrape-logs.py
@@ -2,36 +2,67 @@
 # -*- coding: utf-8 -*-
 import os
 
+def addToDict(dict, case):
+    if case in dict:
+        dict[case] = dict[case] + 1
+    else:
+        dict[case] = 1
+
+def sortDictByFreq(dict):
+    return sorted(dict.items(), key=lambda kv: kv[1], reverse=True)
+
+###### MAIN STARTS HERE ######
+failure_text = '31mâœ—'
+pass_text = '32mâœ“'
+endline = '[39m'
+# setup control and count all tests for a failed run
+allTests = {}
+# open file as (r)ead-only and in (b)inary
+with open('failure-logs/3746.txt', 'rb') as dataFile:
+    lines = dataFile.readlines()
+    # look for pass/fail text on each line and strip the content from that text and the endline character
+    for line in lines:
+        if line.__contains__(failure_text):
+            text_case = line.split(failure_text)[1].split(endline)[0]
+
+            addToDict(allTests, text_case)
+
+        if line.__contains__(pass_text):
+            text_case = line.split(pass_text)[1].split(endline)[0]
+
+            addToDict(allTests, text_case)
+
+    dataFile.closed
+# no need to write out the control list
+# writeDataToFile('all.txt', sortDictByFreq(allTests))
+
 failedTests = {}
-def readDataFromFile(fileName):
-    # open file as (r)ead-only and in (b)inary
-    with open(fileName, 'rb') as dataFile:
-        # failedTests = {}
+files_read = 0
+# for each file in folder:
+for filename in os.listdir('error-failure-logs'):
+    with open('error-failure-logs/' + filename, 'rb') as dataFile:
         lines = dataFile.readlines()
-        i = 0
         for line in lines:
-            # character for failed test
-            failure_text = '31mâœ—'
             if line.__contains__(failure_text):
-                text_case = line.split(failure_text)[1].split('[39m')[0]
+                text_case = line.split(failure_text)[1].split(endline)[0]
 
-                if text_case in failedTests:
-                    failedTests[text_case] = failedTests[text_case] + 1
-                else:
-                    failedTests[text_case] = 1
+                addToDict(failedTests, text_case)
 
+        files_read = files_read + 1
         dataFile.closed
 
-def writeDataToFile(fileName):
-    with open(fileName, 'wb') as output:
-        # sort the list by season value
-        for row in failedTests:
-            output.write( row + '| ' + str(failedTests[row]) + '\n')
+failedTestsTuples = []
+# calculate the failure rate before sorting content for output
+for row in failedTests:
+    test_case = row
+    occurrences = failedTests[row]
+    perbuild = str(allTests[test_case]) + ' (' + str(allTests[test_case] * files_read) + ')'
+    rate = round(float(occurrences)/float(allTests[test_case] * files_read) * 100, 2)
+    failedTestsTuples.append( (test_case, occurrences, perbuild, rate) )
 
-        output.closed
+with open('error-failures.txt', 'wb') as output:
+    # sort the list by "failure rate"
+    for row in sorted(failedTestsTuples, key=lambda kv: kv[3], reverse=True):
+        output.write( row[0] + '| ' + str(row[1]) + '| ' + str(row[2]) + '| ' + str(row[3]) + '%\n')
 
-# for each file in folder:
-for filename in os.listdir('failure-logs'):
-    print filename
-    readDataFromFile('failure-logs/' + filename)
-writeDataToFile('failures.txt')
+    output.closed
