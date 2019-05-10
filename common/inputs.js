fa5731215eaff46e1b4d0d3454eae6aef4caac99
Joshua Chudy <jchudy@isi.edu>
2019-05-10 16:04:57 -0700

    revert previous commit: facet panel is NOW closed by default in search popup modals

--- common/inputs.js
+++ common/inputs.js
@@ -417,7 +417,7 @@
                     params.selectedRows = [];
                     params.selectMode = modalBox.singleSelectMode;
                     params.showFaceting = true;
-                    params.facetPanelOpen = true;
+                    params.facetPanelOpen = false;
 
                     modalUtils.showModal({
                         animation: false,
