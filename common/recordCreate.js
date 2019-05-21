7c8470948551a0ce47c2c86b28a15b93bdea04d0
Joshua Chudy <jchudy@isi.edu>
2019-05-21 15:56:07 -0700

    added functionality to create/update callback to add a link to the alert message that redirects to record app

--- common/recordCreate.js
+++ common/recordCreate.js
@@ -245,9 +245,13 @@
                         if (!session && exception instanceof ERMrest.ConflictError) throw new ERMrest.UnauthorizedError();
                         // append link to end of alert.
                         if (exception instanceof ERMrest.DuplicateConflictError) {
-                            exception.message += " Click here to see the conflicting record that already exists."
+                            ERMrest.resolve(exception.uri).then(function (ref) {
+                                exception.message += ' Click <a href="' + ref.contextualize.detailed.appLink + '">here</a> to see the conflicting record that already exists.';
+                                AlertsService.addAlert(exception.message, 'error');
+                            });
+                        } else {
+                            AlertsService.addAlert(exception.message, (exception instanceof ERMrest.NoDataChangedError ? 'warning' : 'error') );
                         }
-                        AlertsService.addAlert(exception.message, (exception instanceof ERMrest.NoDataChangedError ? 'warning' : 'error') );
                     });
                 });
 
