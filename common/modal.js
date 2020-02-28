ec53f098888f9af786eacf80c519d3bf7a1a9a36
Joshua Chudy <jchudy@isi.edu>
2020-02-27 16:11:37 -0800

    found a bug that I introduced that would cause the link to not be a link after the profile modal was opened, closed, then opened again. We were stripping the id value and replacing it in the hash rather than storing it as a separate variable

--- common/modal.js
+++ common/modal.js
@@ -351,7 +351,7 @@
         for(var i = 0; i<session.attributes.length; i++){
             if(session.attributes[i].display_name && session.attributes[i].display_name !== user.display_name && vm.identities.indexOf(session.attributes[i].id) == -1){
                 if (session.attributes[i].id.indexOf("https://auth.globus.org/") === 0) {
-                    session.attributes[i].id = session.attributes[i].id.substring(24);
+                    session.attributes[i].truncatedId = session.attributes[i].id.substring(24);
                     vm.globusGroupList.push(session.attributes[i]);
                 } else {
                     vm.otherGroups.push(session.attributes[i]);
