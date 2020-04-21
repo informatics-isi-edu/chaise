31f2675ef2d1a244ed70980e0ca91ef1a0d59d64
Joshua Chudy <jchudy@isi.edu>
2020-04-20 18:04:54 -0700

    need to be dynamic since travis creates new RIDs every time

--- test/e2e/specs/default-config/recordset/add.spec.js
+++ test/e2e/specs/default-config/recordset/add.spec.js
@@ -26,11 +26,17 @@ describe('Recordset add record,', function() {
     });
 
     it("verify view details link, search for a term, then verify view details link has changed", function () {
+
+
         var baseUrl = '/record/#' + browser.params.catalogId + "/" + testParams.schemaName + ":" + testParams.table_name + "/RID=";
 
         chaisePage.recordsetPage.getRows().then(function (rows) {
+            var dataRow = browser.params.entities[testParams.schemaName][testParams.table_name].find(function (entity) {
+                return entity.id == 2003;
+            });
             // get first row view details button
-            expect(rows[0].element(by.css('.view-action-button')).getAttribute("href")).toContain(baseUrl + "1Z6", "View button url is incorrect before searching set");
+            console.log(baseUrl + dataRow.RID);
+            expect(rows[0].element(by.css('.view-action-button')).getAttribute("href")).toContain(baseUrl + dataRow.RID, "View button url is incorrect before searching set");
 
             // search for a row that is not the first one after sorting
             chaisePage.recordsetPage.getMainSearchInput().sendKeys('hilton');
@@ -40,8 +46,13 @@ describe('Recordset add record,', function() {
             return chaisePage.recordsetPage.getRows();
         }).then(function(rows) {
             expect(rows.length).toBe(1);
+
+            var dataRow = browser.params.entities[testParams.schemaName][testParams.table_name].find(function (entity) {
+                return entity.id == 4004;
+            });
             // get first row view details button
-            expect(rows[0].element(by.css('.view-action-button')).getAttribute("href")).toContain(baseUrl + "1ZE", "View button url is incorrect after searching set");
+            console.log(baseUrl + dataRow.RID);
+            expect(rows[0].element(by.css('.view-action-button')).getAttribute("href")).toContain(baseUrl + dataRow.RID, "View button url is incorrect after searching set");
 
             // clear search
             return chaisePage.recordsetPage.getSearchClearButton().click();
