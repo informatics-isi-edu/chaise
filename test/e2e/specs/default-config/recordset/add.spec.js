2cdfff6a8faa6d232e15113212dbe430df05a32d
Joshua Chudy <jchudy@isi.edu>
2020-05-07 15:17:23 -0700

    added test case to check for ..more and ..less after clicking on the text link

--- test/e2e/specs/default-config/recordset/add.spec.js
+++ test/e2e/specs/default-config/recordset/add.spec.js
@@ -25,9 +25,24 @@ describe('Recordset add record,', function() {
 
     });
 
+    it("verify the text is truncated properly, then not truncated after clicking 'more'", function () {
+        var testCell;
+        chaisePage.recordsetPage.getRows().then(function (rows) {
+            return chaisePage.recordsetPage.getRowCells(rows[0]);
+        }).then(function (cells) {
+            testCell = cells[4];
+            expect(testCell.getText()).toContain("... more");
+            return testCell.element(by.css(".readmore"));
+        }).then(function (readmore) {
+            return readmore.click();
+        }).then(function () {
+            expect(testCell.getText()).toContain("... less");
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+
     it("verify view details link, search for a term, then verify view details link has changed", function () {
-
-
         var baseUrl = '/record/#' + browser.params.catalogId + "/" + testParams.schemaName + ":" + testParams.table_name + "/RID=";
 
         chaisePage.recordsetPage.getRows().then(function (rows) {
@@ -35,7 +50,6 @@ describe('Recordset add record,', function() {
                 return entity.id == 2003;
             });
             // get first row view details button
-            console.log(baseUrl + dataRow.RID);
             expect(rows[0].element(by.css('.view-action-button')).getAttribute("href")).toContain(baseUrl + dataRow.RID, "View button url is incorrect before searching set");
 
             // search for a row that is not the first one after sorting
@@ -51,7 +65,6 @@ describe('Recordset add record,', function() {
                 return entity.id == 4004;
             });
             // get first row view details button
-            console.log(baseUrl + dataRow.RID);
             expect(rows[0].element(by.css('.view-action-button')).getAttribute("href")).toContain(baseUrl + dataRow.RID, "View button url is incorrect after searching set");
 
             // clear search
