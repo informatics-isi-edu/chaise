657228c143b8057b479dce6bc55d278557c39cc9
Joshua Chudy <jchudy@isi.edu>
2020-08-21 13:45:13 -0700

    after adding condition to show a 'url link' only if url is defined

--- test/e2e/specs/all-features/chaise-config.js
+++ test/e2e/specs/all-features/chaise-config.js
@@ -37,6 +37,7 @@ var chaiseConfig = {
             },
             {
                 name: "Records",
+                url: "/chaise/search/#1/isa:dataset",
                 acls: {
                     show: ["https://auth.globus.org/9d596ac6-22b9-11e6-b519-22000aef184d"],
                     enable: []
