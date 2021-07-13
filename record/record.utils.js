3209fc1c42422aa0241111e11744651c79b0fd29
KPherwani <karishma.pherwani21@gmail.com>
2021-07-13 16:56:23 -0700

    1801 - if config does not exist then append JSON-LD anyway if annotation is setup

--- record/record.utils.js
+++ record/record.utils.js
@@ -641,45 +641,50 @@
                 DataUtils.isObjectAndNotNull(googleDatasetConfigs[catalogID][schemaName][tableName])
                 ) {
                 var currConfig = googleDatasetConfigs[catalogID][schemaName][tableName];
-                var columns = currConfig.columns;
-                var allValuesArray = currConfig.values;
-                var valueInTuple;
-                
-                if (Array.isArray(columns) && Array.isArray(allValuesArray) && 
-                (columns.length == allValuesArray.length || (columns.length == 1 && allValuesArray.length > 0))) {
-                    var isValuePresentInTuple = false;
-                    // 2D Array of values
-                    if(Array.isArray(allValuesArray[0])) {
-                        columns.some(function(col, index) {
-                            valueInTuple = tuple.data[col];
-                            if (valueInTuple && allValuesArray[index].includes(valueInTuple)) {
+                if(currConfig) {
+                    var columns = currConfig.columns;
+                    var allValuesArray = currConfig.values;
+                    var valueInTuple;
+                    
+                    if (Array.isArray(columns) && Array.isArray(allValuesArray) && 
+                    (columns.length == allValuesArray.length || (columns.length == 1 && allValuesArray.length > 0))) {
+                        var isValuePresentInTuple = false;
+                        // 2D Array of values
+                        // { "columns": ["RID", "PID"], "values": [["RID_v1", "RID_v2"], ["PID_v1", "PID_v2"]]}
+                        if(Array.isArray(allValuesArray[0])) {
+                            columns.some(function(col, index) {
+                                valueInTuple = tuple.data[col];
+                                if (valueInTuple && allValuesArray[index].includes(valueInTuple)) {
+                                    isValuePresentInTuple = true;
+                                    return true;
+                                }
+                            });
+                        }
+                        // allowed case of 1D array of values
+                        // { "columns": ["RID"], "values": ["RID_v1", "RID_v2"]}
+                        else {
+                            valueInTuple = tuple.data[columns[0]];
+                            if (valueInTuple && allValuesArray.includes(valueInTuple)) {
                                 isValuePresentInTuple = true;
-                                return true;
-                            }
-                        });
-                    }
-                    // allowed case of 1D array of values
-                    // { "columns": ["RID"], "values": ["RID_v1", "RID_v2"]}
-                    else {
-                        valueInTuple = tuple.data[columns[0]];
-                        if (valueInTuple && allValuesArray.includes(valueInTuple)) {
-                            isValuePresentInTuple = true;
-                        }
-                    }
-                    if (isValuePresentInTuple) {
-                        // use ermrestjs and attach to dom
-                        var metadata = $rootScope.reference.googleDatasetMetadata;
-                        if (DataUtils.isObjectAndNotNull(metadata)) {
-                            metadata = metadata.compute(tuple, $rootScope.templateVariables);
-                            if (DataUtils.isObjectAndNotNull(metadata)) {
-                                metadata.url = window.location.href;
-                                var script = document.createElement('script');
-                                script.setAttribute('type', 'application/ld+json');
-                                script.textContent = JSON.stringify(metadata, null, 4);
-                                document.head.appendChild(script);
                             }
                         }
+                        if (!isValuePresentInTuple) {
+                            return;
+                        }
                     }
+                }
+            }
+
+            // use ermrestjs and attach to dom if annotation exists
+            var metadata = $rootScope.reference.googleDatasetMetadata;
+            if (DataUtils.isObjectAndNotNull(metadata)) {
+                metadata = metadata.compute(tuple, $rootScope.templateVariables);
+                if (DataUtils.isObjectAndNotNull(metadata)) {
+                    metadata.url = window.location.href;
+                    var script = document.createElement('script');
+                    script.setAttribute('type', 'application/ld+json');
+                    script.textContent = JSON.stringify(metadata, null, 4);
+                    document.head.appendChild(script);
                 }
             }
         }
