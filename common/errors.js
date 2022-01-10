7bb2fe0bf4d6dae7b8bd3613779d33a99425c3ab
Aref Shafaei <aref.shafaei@gmail.com>
2022-01-10 15:20:06 -0800

    make sure terminal error is displayed for raw 409 errors
    and not for specific integiry and duplicate conflict errors where
    the message is readable.

--- common/errors.js
+++ common/errors.js
@@ -620,10 +620,16 @@
                 if (DataUtils.isObjectAndKeyDefined(exception.errorData, 'gotoTableDisplayname')) pageName = exception.errorData.gotoTableDisplayname;
                 if (DataUtils.isObjectAndKeyDefined(exception.errorData, 'redirectUrl')) redirectLink = exception.errorData.redirectUrl;
 
-                // the conflict error message is not readable by users,
+                // the raw conflict error message is not readable by users,
                 // so we're going to show a terminal error instead
                 // NOTE we might want to do the same thing for all the other ermrest HTTP errors
-                if (exception instanceof ERMrest.ConflictError) {
+                if (
+                    exception instanceof ERMrest.ConflictError &&
+                    !(
+                        exception instanceof ERMrest.IntegrityConflictError ||
+                        exception instanceof ERMrest.DuplicateConflictError
+                    )
+                ) {
                     message = errorMessages.systemAdminMessage;
                 }
             }
