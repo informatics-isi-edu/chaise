1d1a754219e5b3cffc15d2739d978903803d0875
Joshua Chudy <jchudy@isi.edu>
2022-03-24 13:41:08 -0700

    linter change

--- src/components/login-dropdown.tsx
+++ src/components/login-dropdown.tsx
@@ -13,73 +13,75 @@ import { windowRef } from '@chaise/utils/window-ref';
 //       hasn't been updated to use the "types" or set default types on menu ingest
 const ChaiseLoginDropdown = ({
   menu, openProfileCb, parentDropdown
-}: any): JSX.Element => menu.map((child: any, index: number) => {
-  if (!MenuUtils.canShow(child) || !child.isValid) return;
-
+}: any): JSX.Element => {
   const dropdownWrapper = useRef<any>(null);
 
-  switch (child.type) {
-    case 'header':
-      return (
-        <NavDropdown.Header
-          key={index}
-          className='chaise-dropdown-header'
-          dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
-        />
-      );
-    case 'menu':
-      let dropEnd = true;
-      const winWidth = windowRef.innerWidth;
+  return menu.map((child: any, index: number) => {
+    if (!MenuUtils.canShow(child) || !child.isValid) return;
 
-      // parentDropdown.current is the parent menu option that toggles open the menu below
-      // this menu is generated when the parent dropdown is opened so we don't know how wide the child will be on open
-      // 
-      if (parentDropdown && (Math.round(winWidth - parentDropdown.current.getBoundingClientRect().right) < parentDropdown.current.clientWidth*2) ) dropEnd = false;
+    switch (child.type) {
+      case 'header':
+        return (
+          <NavDropdown.Header
+            key={index}
+            className='chaise-dropdown-header'
+            dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
+          />
+        );
+      case 'menu':
+        let dropEnd = true;
+        const winWidth = windowRef.innerWidth;
 
-      return (
-        <Dropdown key={index} drop={dropEnd ? 'end' : 'start'} className='dropdown-submenu' ref={dropdownWrapper}>
-          <Dropdown.Toggle
-            as='a'
-            variant='dark'
+        // parentDropdown.current is the parent menu option that toggles open the menu below
+        // this menu is generated when the parent dropdown is opened so we don't know how wide the child will be on open
+        // check if opening the menu twice would push off the screen to have the next dropdown open left instead
+        if (parentDropdown && (Math.round(winWidth - parentDropdown.current.getBoundingClientRect().right) < parentDropdown.current.clientWidth * 2)) dropEnd = false;
+
+        return (
+          <Dropdown key={index} drop={dropEnd ? 'end' : 'start'} className='dropdown-submenu' ref={dropdownWrapper}>
+            <Dropdown.Toggle
+              as='a'
+              variant='dark'
+              className={MenuUtils.menuItemClasses(child, true)}
+              dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
+            />
+            <Dropdown.Menu>
+              <ChaiseLoginDropdown menu={child.children} openProfileCb={openProfileCb} parentDropdown={dropdownWrapper}></ChaiseLoginDropdown>
+            </Dropdown.Menu>
+          </Dropdown>
+        );
+      case 'url':
+        return (
+          <NavDropdown.Item
+            key={index}
+            href={child.url}
+            target={child.newTab ? '_blank' : '_self'}
             className={MenuUtils.menuItemClasses(child, true)}
             dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
           />
-          <Dropdown.Menu>
-            <ChaiseLoginDropdown menu={child.children} openProfileCb={openProfileCb} parentDropdown={dropdownWrapper}></ChaiseLoginDropdown>
-          </Dropdown.Menu>
-        </Dropdown>
-      );
-    case 'url':
-      return (
-        <NavDropdown.Item
-          key={index}
-          href={child.url}
-          target={child.newTab ? '_blank' : '_self'}
-          className={MenuUtils.menuItemClasses(child, true)}
-          dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
-        />
-      );
-    case 'my_profile':
-      return (
-        <NavDropdown.Item
-          id='profile-link'
-          key={index}
-          onClick={openProfileCb}
-          dangerouslySetInnerHTML={{ __html: child.nameMarkdownPattern ? MenuUtils.renderName(child) : 'My Profile' }}
-        />
-      )
-    case 'logout':
-      return (
-        <NavDropdown.Item
-          id='logout-link'
-          key={index}
-          onClick={MenuUtils.logout}
-          dangerouslySetInnerHTML={{ __html: child.nameMarkdownPattern ? MenuUtils.renderName(child) : 'Log Out' }}
-        />
-      )
-    default:
-      return;
-  }
-});
+        );
+      case 'my_profile':
+        return (
+          <NavDropdown.Item
+            id='profile-link'
+            key={index}
+            onClick={openProfileCb}
+            dangerouslySetInnerHTML={{ __html: child.nameMarkdownPattern ? MenuUtils.renderName(child) : 'My Profile' }}
+          />
+        )
+      case 'logout':
+        return (
+          <NavDropdown.Item
+            id='logout-link'
+            key={index}
+            onClick={MenuUtils.logout}
+            dangerouslySetInnerHTML={{ __html: child.nameMarkdownPattern ? MenuUtils.renderName(child) : 'Log Out' }}
+          />
+        )
+      default:
+        return;
+    }
+  });
+}
 
 export default ChaiseLoginDropdown;
--- src/components/nav-dropdown.tsx
+++ src/components/nav-dropdown.tsx
@@ -11,61 +11,63 @@ import { windowRef } from '@chaise/utils/window-ref';
 // TODO: make a menu object interface
 const ChaiseNavDropdown = ({
   menu, parentDropdown
-}: any): JSX.Element => menu.map((child: any, index: number) => {
-  if (!MenuUtils.canShow(child)) return;
-
+}: any): JSX.Element => {
   const dropdownWrapper = useRef<any>(null);
 
-  // create an unclickable header
-  if (child.header == true && !child.children && !child.url) {
-    return (
-      <NavDropdown.Header 
-        key={index} 
-        className='chaise-dropdown-header'
-        dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
-      />
-    );
-  }
+  return menu.map((child: any, index: number) => {
+    if (!MenuUtils.canShow(child)) return;
 
-  // TODO: onClick logging
-  if ((!child.children && child.url) || !MenuUtils.canEnable(child)) {
-    return (
-      <NavDropdown.Item
-        key={index}
-        href={child.url}
-        target={child.newTab ? '_blank' : '_self'}
-        className={MenuUtils.menuItemClasses(child, true)}
-        dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
-      />
-    );
-  }
-
-  if (child.children && MenuUtils.canEnable(child)) {
-    let dropEnd = true;
-    const winWidth = windowRef.innerWidth;
-
-    // parentDropdown.current is the parent menu option that toggles open the menu below
-    // this menu is generated when the parent dropdown is opened so we don't know how wide the child will be on open
-    // check if opening the menu twice would push off the screen to have the next dropdowns open left instead
-    if (parentDropdown && (Math.round(winWidth - parentDropdown.current.getBoundingClientRect().right) < parentDropdown.current.offsetWidth*2) ) dropEnd = false;
-
-    return (
-      <Dropdown key={index} drop={dropEnd ? 'end' : 'start'} className='dropdown-submenu' ref={dropdownWrapper}>
-        <Dropdown.Toggle 
-          as='a'
-          variant='dark' 
-          className={MenuUtils.menuItemClasses(child, true)} 
-          dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }} 
+    // create an unclickable header
+    if (child.header == true && !child.children && !child.url) {
+      return (
+        <NavDropdown.Header
+          key={index}
+          className='chaise-dropdown-header'
+          dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
         />
-        <Dropdown.Menu>
-          <ChaiseNavDropdown menu={child.children} parentDropdown={dropdownWrapper}></ChaiseNavDropdown>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-    // TODO: navbar-header-container
-  }
+      );
+    }
 
-  return;
-});
+    // TODO: onClick logging
+    if ((!child.children && child.url) || !MenuUtils.canEnable(child)) {
+      return (
+        <NavDropdown.Item
+          key={index}
+          href={child.url}
+          target={child.newTab ? '_blank' : '_self'}
+          className={MenuUtils.menuItemClasses(child, true)}
+          dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
+        />
+      );
+    }
+
+    if (child.children && MenuUtils.canEnable(child)) {
+      let dropEnd = true;
+      const winWidth = windowRef.innerWidth;
+
+      // parentDropdown.current is the parent menu option that toggles open the menu below
+      // this menu is generated when the parent dropdown is opened so we don't know how wide the child will be on open
+      // check if opening the menu twice would push off the screen to have the next dropdown open left instead
+      if (parentDropdown && (Math.round(winWidth - parentDropdown.current.getBoundingClientRect().right) < parentDropdown.current.offsetWidth * 2)) dropEnd = false;
+
+      return (
+        <Dropdown key={index} drop={dropEnd ? 'end' : 'start'} className='dropdown-submenu' ref={dropdownWrapper}>
+          <Dropdown.Toggle
+            as='a'
+            variant='dark'
+            className={MenuUtils.menuItemClasses(child, true)}
+            dangerouslySetInnerHTML={{ __html: MenuUtils.renderName(child) }}
+          />
+          <Dropdown.Menu>
+            <ChaiseNavDropdown menu={child.children} parentDropdown={dropdownWrapper}></ChaiseNavDropdown>
+          </Dropdown.Menu>
+        </Dropdown>
+      );
+      // TODO: navbar-header-container
+    }
+
+    return;
+  });
+}
 
 export default ChaiseNavDropdown;
