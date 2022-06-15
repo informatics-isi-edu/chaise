14750457eb7e15d63d73e91f72369d96a1708875
ravishkg <kalapana@usc.edu>
2022-06-14 17:58:05 -0700

    panel header changes added

--- src/components/faceting.tsx
+++ src/components/faceting.tsx
@@ -31,12 +31,13 @@ const Faceting = ({
   };
 
   /**
-   * 
-   * @param param0 prop that holds content to show as header
-   * @param param1 showTooltip prop to enable tooltip for header text
+   * @param key key prop to uniquely identify panel header item 
+   * @param headerText headerText prop that holds content to show as header
+   * @param showTooltipIcon prop to enable tooltip for header text
+   * @param tooltipContent text to be shown on hover of header text
    * @returns FacetHeader Component
    */
-  const FacetHeader = ({ key, content, showTooltip, tooltipContent }) => {
+  const FacetHeader = ({ key, headerText, showTooltipIcon, tooltipContent }) => {
     /**
      * @contentRef variable to store ref of facet header text
      * @show state variable to control whether to show tooltip or not
@@ -51,27 +52,43 @@ const Faceting = ({
       }
       return false; 
     }
-    
+
     return (
       <OverlayTrigger
+        trigger='hover'
         placement='right'
-        overlay={<Tooltip><DisplayValue value={tooltipContent} /></Tooltip>}
-        onToggle={(nextshow: boolean) => {
+        overlay={
+          <Tooltip
+            style={{ maxWidth: '50%', whiteSpace: 'nowrap' }}
+          >
+            {contentRef && contentRef.current && isTextOverflow(contentRef.current) ? (
+              <>
+                <DisplayValue value={headerText}/>
+                <br/>
+                {tooltipContent}
+              </>
+            ) : (tooltipContent ? tooltipContent : <DisplayValue value={headerText}/>)}
+          </Tooltip>
+        }
+        onToggle={(nextshow: boolean) => { 
+          
+          // Bootstrap onToggle prop to make tooltip visible or hidden
           if (contentRef && contentRef.current) {
             const isOverflow = isTextOverflow(contentRef.current);
+
             // If either text overflow or showtooltip is true, show tooltip to right of the content
-            setShow((isOverflow || showTooltip) && nextshow);
+            setShow((isOverflow || showTooltipIcon) && nextshow);
           }
-        }}
-        onExiting={() => {
-          setShow(false);
         }}
         show={show}
       >
         <div className='accordion-toggle ellipsis' id={key}>
           <div ref={contentRef} className='facet-header-text ellipsis'>
-            <DisplayValue value={content} />
-            <span className='chaise-icon-for-tooltip align-center-icon'></span>
+            <DisplayValue value={headerText} />
+            {/* Condition to show tooltip icon */}
+            {showTooltipIcon && (
+              <span className='chaise-icon-for-tooltip align-center-icon'></span>
+            )}
           </div>
           <span className='facet-header-icon'></span>
         </div>
@@ -83,13 +100,16 @@ const Faceting = ({
     return reference.facetColumns.map((fc: any, index: number) => {
       return (
         <Accordion.Item eventKey={index + ''} key={index} className='facet-panel'>
+          {/* TODO: On Click of header text in facet panel, because of the overlay 
+            it wont trigger accordion to open. Aref is handling that part (handling accordion 
+            open and close manually) */}
           <Accordion.Header>
-              <FacetHeader 
-                key={`fc-heading-${index}`}
-                content={fc.displayname} 
-                showTooltip={true} 
-                tooltipContent={fc.displayname}
-              />
+            <FacetHeader 
+              key={`fc-heading-${index}`}
+              headerText={fc.displayname} 
+              showTooltipIcon={fc.comment ? true : false} 
+              tooltipContent={fc.comment}
+            />
           </Accordion.Header>
           <Accordion.Body>
             {renderFacet(fc, index)}
