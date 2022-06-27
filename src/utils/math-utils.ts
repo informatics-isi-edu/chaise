0bb7677016dec98d530d1e8136bab896f8aacb13
ravishkg <kalapana@usc.edu>
2022-06-27 13:30:06 -0700

    added return types

--- src/utils/math-utils.ts
+++ src/utils/math-utils.ts
@@ -27,7 +27,7 @@ function uuidS4() {
  * @param max maximum number
  * @returns {number} random number generated
  */
-export function getRandomInt(min: number, max: number) {
+export function getRandomInt(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
