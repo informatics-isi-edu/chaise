0a3ae9f9ff218239dc86faf476164f174cb11552
Aref Shafaei <aref.shafaei@gmail.com>
2022-06-30 17:38:28 -0700

    add show more/less logic to selected chiclets

--- src/components/selected-chiclets.tsx
+++ src/components/selected-chiclets.tsx
@@ -1,46 +1,8 @@
-import { Displayname } from '@isrd-isi-edu/chaise/src/models/displayname'
 import { SelectedChiclet } from '@isrd-isi-edu/chaise/src/models/recordset'
 import DisplayValue from '@isrd-isi-edu/chaise/src/components/display-value';
 import ChaiseTooltip from '@isrd-isi-edu/chaise/src/components/tooltip';
 import { useLayoutEffect, useRef, useState } from 'react';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-
-
-type SelectedChicletNameProp = {
-  displayname: Displayname
-}
-
-// TODO this can be a reusable component for all the places that we want to show ellupsis
-const SelectedChicletName = ({
-  displayname
-}: SelectedChicletNameProp) : JSX.Element => {
-
-  const wrapper = useRef<HTMLSpanElement>(null);
-  const [showTooltip, setShowTooltip] = useState(false);
-
-  const inner = <DisplayValue specialNullEmpty={true} value={displayname} />;
-  return (
-    <OverlayTrigger
-      trigger={['hover', 'focus']}
-      placement='bottom-start'
-      overlay={<Tooltip>{inner}</Tooltip>}
-      show={showTooltip}
-      onToggle={(nextshow: boolean) => {
-        if (!wrapper.current) return;
-
-        const el = wrapper.current as HTMLElement;
-        const overflow = el.scrollWidth > el.offsetWidth;
-
-        setShowTooltip(nextshow && overflow);
-      }}
-    >
-      <span className='selected-chiclet-name' ref={wrapper}>
-        {inner}
-      </span>
-    </OverlayTrigger>
-  )
-};
+import { ResizeSensor } from 'css-element-queries';
 
 
 type SelectedChicletProps = {
@@ -55,49 +17,98 @@ type SelectedChicletProps = {
 const SelectedChiclets = ({
   rows,
   removeCallback
-}: SelectedChicletProps) : JSX.Element => {
-  // TODO show more, show less logic
+}: SelectedChicletProps): JSX.Element => {
+  const [overflow, setOverflow] = useState(false);
+  const [showMore, setShowMore] = useState(false);
 
   const container = useRef<HTMLDivElement>(null);
 
+  // 3 rows where each row has the height of 25px
+  const maxHeight = 25 * 3;
+
   useLayoutEffect(() => {
-    if (!container) return;
+    if (!container || !container.current) return;
+
+    setOverflow(container.current.scrollHeight > maxHeight);
 
   }, [rows]);
+
+  // TODO it should be dynamic
+  const checkHeight = () => {
+    let cachedHeight : number = -1, tm : any;
+    return new ResizeSensor(container.current as Element, (dimension) => {
+      if (dimension.height === cachedHeight) return;
+      cachedHeight = dimension.height;
+      if (tm) clearTimeout(tm);
+      tm = setTimeout(() => {
+        setOverflow(container.current!.scrollHeight > maxHeight);
+      }, 500);
+    });
+  };
+
+  const renderShowMoreLessBtn = (isMoreBtn: boolean) => (
+    <ChaiseTooltip
+      placement='bottom-start'
+      tooltip={isMoreBtn? 'Click to show all selected values.' : 'Click to hide some selected values.'}
+    >
+      <button
+        className={`chaise-btn chaise-btn-tertiary selected-chiclet-btn ${isMoreBtn ? 'show-more-btn' : 'show-less-btn'}`}
+        onClick={() => setShowMore(isMoreBtn)}
+      >
+        ...Show {isMoreBtn ? 'more' : 'less'}
+      </button>
+    </ChaiseTooltip>
+  )
+
+  const renderClearBtn = () => (
+    <ChaiseTooltip
+      placement='bottom-start'
+      tooltip='Clear all selected rows'
+    >
+      <button
+        className='chaise-btn chaise-btn-tertiary clear-all-btn selected-chiclet-btn'
+        onClick={(event) => removeCallback(null, event)}
+      >
+        <span>Clear selection</span>
+      </button>
+    </ChaiseTooltip>
+  )
 
   if (rows.length === 0) {
     return <></>;
   }
 
+  let containerStyles = {};
+  if (overflow && !showMore) {
+    containerStyles = {maxHeight, overflowY: 'hidden'};
+  }
+
   return (
-    <div className='chiclets-container selected-chiclets' ref={container}>
-      {rows.map((row: any, index: number) => (
-        <div key={row.uniqueId} className='selected-chiclet'>
-          <ChaiseTooltip
-            placement='bottom-start'
-            tooltip='Clear selected row'
-          >
-            <span className='selected-chiclet-remove' onClick={(event) => removeCallback(row, event)}>
-              <i className='fa-solid fa-xmark' />
-            </span>
-          </ChaiseTooltip>
-          <SelectedChicletName displayname={row.displayname} />
-        </div>
-      ))}
-      {/* using the same class to ensure consistent position and spacing */}
-      <div className='selected-chiclet'>
-        <ChaiseTooltip
-          placement='bottom-start'
-          tooltip='Clear all selected rows'
-        >
-          <button
-            className='chaise-btn chaise-btn-tertiary clear-all-btn'
-            onClick={(event) => removeCallback(null, event)}
-          >
-            <span>Clear selection</span>
-          </button>
-        </ChaiseTooltip>
+    <div className='chiclets-container'>
+      <div className='selected-chiclets' ref={container} style={containerStyles}>
+        {rows.map((row: any, index: number) => (
+          <div key={row.uniqueId} className='selected-chiclet'>
+            <ChaiseTooltip
+              placement='bottom-start'
+              tooltip='Clear selected row'
+            >
+              <span className='selected-chiclet-remove' onClick={(event) => removeCallback(row, event)}>
+                <i className='fa-solid fa-xmark' />
+              </span>
+            </ChaiseTooltip>
+            <DisplayValue specialNullEmpty={true} value={row.displayname} />
+          </div>
+        ))}
+        {!overflow &&
+          <div className='selected-chiclet'>{renderClearBtn()}</div>
+        }
       </div>
+      {overflow &&
+        <div>
+          <div className='selected-chiclet'>{renderShowMoreLessBtn(!showMore)}</div>
+          <div className='selected-chiclet'>{renderClearBtn()}</div>
+        </div>
+      }
     </div>
   )
 };
