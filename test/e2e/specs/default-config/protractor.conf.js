1708a2c691f607b1ac974daad15e20e1717b7159
Joshua Chudy <jchudy@isi.edu>
2022-08-23 15:44:54 -0700

    only run 6 specs in default config

--- test/e2e/specs/default-config/protractor.conf.js
+++ test/e2e/specs/default-config/protractor.conf.js
@@ -4,7 +4,13 @@ var config = pConfig.getConfig({
     // This config is meant to be run as part of the parallel tests configuration
     configFileName: 'parallel-configs/default-config.dev.json',
     specs: [
-        "*/*.spec.js"
+        // "*/*.spec.js"
+        "record/links.spec.js",
+        "recordedit/add-x-forms.spec.js",
+        "recordedit/immutable-inputs.spec.js",
+        "recordedit/domain-filter.spec.js",
+        "recordedit/remove-edit-form.spec.js",
+        "recordedit/submission-disabled.spec.js"
     ],
     setBaseUrl: function(browser, data) {
         browser.params.url = process.env.CHAISE_BASE_URL;
