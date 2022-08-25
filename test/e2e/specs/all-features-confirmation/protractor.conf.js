4ebb6b85f39dde3a39f7081b3679ce23a4edc28d
Joshua Chudy <jchudy@isi.edu>
2022-08-25 00:20:06 -0700

    full features 5 specs

--- test/e2e/specs/all-features-confirmation/protractor.conf.js
+++ test/e2e/specs/all-features-confirmation/protractor.conf.js
@@ -10,7 +10,6 @@ var config = pConfig.getConfig({
         "record/copy-btn.spec.js",
         "recordedit/permissions-annotation.spec.js",
         "acls/dynamic-acl.spec.js",
-        "recordset/permissions-annotation.spec.js"
     ],
     setBaseUrl: function(browser, data) {
         browser.params.url = process.env.CHAISE_BASE_URL;
