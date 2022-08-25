d25b469e1bfbe14dcd18d2e4a8638f894133c3e0
Joshua Chudy <jchudy@isi.edu>
2022-08-24 19:59:03 -0700

    full features 6 specs

--- test/e2e/specs/all-features-confirmation/protractor.conf.js
+++ test/e2e/specs/all-features-confirmation/protractor.conf.js
@@ -4,7 +4,13 @@ var config = pConfig.getConfig({
     // This config is meant to be run as part of the parallel tests configuration
     configFileName: 'parallel-configs/all-features-confirmation.dev.json',
     specs: [
-        "*/*.spec.js"
+        // "*/*.spec.js"
+        "acls/static-acl.spec.js",
+        "navbar/base-config.spec.js",
+        "record/copy-btn.spec.js",
+        "recordedit/permissions-annotation.spec.js",
+        "acls/dynamic-acl.spec.js",
+        "recordset/permissions-annotation.spec.js"
     ],
     setBaseUrl: function(browser, data) {
         browser.params.url = process.env.CHAISE_BASE_URL;
