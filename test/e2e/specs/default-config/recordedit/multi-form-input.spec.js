77633828f71f7249816e64ca1fe4041289777530
Aref Shafaei <aref.shafaei@gmail.com>
2023-09-13 12:10:27 -0700

    add more info to multi-form-input test spec

--- test/e2e/specs/default-config/recordedit/multi-form-input.spec.js
+++ test/e2e/specs/default-config/recordedit/multi-form-input.spec.js
@@ -9,10 +9,81 @@ const testParams = {
 
 describe('Regarding multi form input and clone button', () => {
   describe('Regarding multi form input,', () => {
-    // TODO
+    describe('in create mode', () => {
+      beforeAll((done) => {
+        chaisePage.navigate(`${browser.params.url}/recordedit/#${browser.params.catalogId}/${testParams.schema_table}`);
+        chaisePage.recordeditPageReady().then(() => {
+          cloneFormInput = chaisePage.recordEditPage.getCloneFormInput();
+          cloneFormSubmitButton = chaisePage.recordEditPage.getCloneFormInputSubmitButton();
+          done();
+        }).catch(chaisePage.catchTestError(done));
+      });
+
+      it('it should not be offered in single mode.', () => {
+        // TODO
+      });
+
+      it('it should be displayed as soon as users added a new form.', () => {
+        // TODO
+      });
+
+      it('it should not be offered for disabled columns.', () => {
+        // TODO
+      });
+
+      it('by default only the first form should be selected.', () => {
+        // TODO
+        // check that is selected
+        // check that adding value and clicking on apply only applies to the first one.
+      });
+
+      it('the newly cloned form should not be selected.', () => {
+        // click on clone and see that it's not selected.
+        // check that adding value and clicking on apply only applies to the first one.
+      });
+
+      /**
+       * TODO more test cases should be added here...
+       * feel free to change any of the tests that I mentioned above. as long as you've covered all the features,
+       * it's ok to not go exactly how I described this.
+       *
+       * You can also take a look at add-x-forms.spec.ignored.js which was the previous test cases for this feature.
+       * In there you can see that we're testing select-all for multiple column types. you should do something
+       * similar here. we should remove that spec when you're done with the changes. I just left it so you can take a
+       * look at an example.
+       */
+    });
+
+    /**
+     * the tests below are just to make sure edit works as well. the bulk of test cases should be added above.
+     */
+    describe('in edit mode', () => {
+      it('in single edit the toggle button should not be available.', () => {
+        // TODO
+        // go to a single edit page and make sure the button is not available.
+      });
+
+      describe('in multi edit', () => {
+        beforeAll(() => {
+          // TODO go to a multiedit page
+        });
+
+        it('the toggle button should be offered on all non-disabled columns.', () => {
+          // TODO
+          // make sure it's not available for the disabled column
+          // make sure it's available it's availbale for one of the columns
+        });
+
+        it('user should be able to use this control to change all values for a column.', () => {
+          // TODO
+          // click on the toggle for that one column, select all, change value, and then submit.
+        });
+      });
+    });
   });
 
-  describe('Regarding clone button, in create mode,', () => {
+  // TODO x should be removed when the tests above are added
+  xdescribe('Regarding clone button, in create mode,', () => {
     let cloneFormInput, cloneFormSubmitButton;
 
     beforeAll((done) => {
