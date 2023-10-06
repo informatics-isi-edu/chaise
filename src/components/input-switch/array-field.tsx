7a5eafbc06f40ecf1d82cf1c3d2c3c3183aa6a70
Aniket Ladukar <ladukaraniket.projects@gmail.com>
2023-10-05 17:47:47 -0700

    fixed Strict Mode related issue with other minor fixes

--- src/components/input-switch/array-field.tsx
+++ src/components/input-switch/array-field.tsx
@@ -11,8 +11,9 @@ import {
   DragDropContext, Draggable, DraggableProvided, DroppableProvided, DropResult
 } from 'react-beautiful-dnd';
 import { useFieldArray, useFormContext, useFormState, useWatch } from 'react-hook-form';
-import { dataFormats } from '../../utils/constants';
-import ChaiseDroppable from '../chaise-droppable';
+import { dataFormats } from '@isrd-isi-edu/chaise/src/utils/constants';
+import ChaiseDroppable from '@isrd-isi-edu/chaise/src/components/chaise-droppable';
+import { RecordeditColumnModel } from '@isrd-isi-edu/chaise/src/models/recordedit';
 
 
 type ArrayFieldProps = InputFieldProps & {
@@ -21,33 +22,22 @@ type ArrayFieldProps = InputFieldProps & {
    */
   baseArrayType: string,
   /**
-   * represents the presence of timezone in case of TimeStamp field
+   * The column model that is used for this input
    */
-  hasTimezone?: boolean
+  columnModel?: RecordeditColumnModel
 };
 
-type RowItem = {
-  id: number,
-  value: string
-}
-
-type Options = {
-  name?: string,
-  type?: EventType,
-  value?: unknown
-  values?: {
-    [x: string]: any
-  }
-}
-
 const ArrayField = (props: ArrayFieldProps): JSX.Element => {
-  const { disableInput, name, baseArrayType } = props;
-  const { register, trigger, control } = useFormContext();
+  const { disableInput, name, baseArrayType, requiredInput, columnModel } = props;
+  const { register, trigger, control, setValue } = useFormContext();
   const { fields, append, remove, move } = useFieldArray({ name: name, control: control });
   /**
    * We use this to keep track of errors in new value input box
    */
   const formState = useFormState({ name: `${name}-new-item` });
+  /**
+   * We use this to keep track of value in new value input box
+   */
   const addNewValue = useWatch({ name: `${name}-new-item` });
 
 
@@ -58,35 +48,46 @@ const ArrayField = (props: ArrayFieldProps): JSX.Element => {
   }
 
   const addItem = (value: any) => {
-    setDisableAddButton(Object.keys(formState.errors).includes(`${name}-new-item`) || getValues(`${name}-new-item`) === undefined || getValues(`${name}-new-item`) === '')
-  }, [formState])
 
     let valueToAdd: any = {
-      "val": value
+      'val': value
     }
 
-    if (getInputType({ name: baseArrayType }) === `timestamp`) {
-      const DATE_TIME_FORMAT = props.hasTimezone ? dataFormats.datetime.return : dataFormats.timestamp;
+    if (getInputType({ name: baseArrayType }) === 'timestamp') {
+
+      const DATE_TIME_FORMAT = columnModel?.column.type.rootName === 'timestamptz' ? dataFormats.datetime.return : dataFormats.timestamp;
       const v = formatDatetime(value, { outputMomentFormat: DATE_TIME_FORMAT })
 
-      valueToAdd["val-date"] = v?.date
-      valueToAdd["val-time"] = v?.time
+      valueToAdd = {
+        'val': v?.datetime,
+        'val-date': v?.date,
+        'val-time': v?.time
+      }
     }
 
-  //TODO - verify types
     append(valueToAdd)
   }
 
+  const clearAddNewField = () => {
+    setValue(`${name}-new-item`, '')
+
+    if (getInputType({ name: baseArrayType }) === 'timestamp') {
+      setValue(`${name}-new-item-date`, '')
+      setValue(`${name}-new-item-time`, '')
+    }
+  }
 
   const DraggableItemRenderer = (item: any, index: number, disableInput: boolean | undefined) => {
 
     return <Draggable key={item.id} draggableId={name + '-' + item.id.toString()} index={index} isDragDisabled={disableInput}>
       {
         (provided: DraggableProvided) => {
-          return <li className={`item ${baseArrayType}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} key={item.id}>
+          return <li className={`item ${getInputType({ name: baseArrayType })} ${!disableInput ? 'add-padding-bottom' : ''}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} key={item.id}>
+
             <div className='move-icon'>
               <i className='fa-solid fa-grip-vertical'></i>
             </div>
+
 
             <div className='item-input'>
               <InputSwitch
@@ -123,44 +124,45 @@ const ArrayField = (props: ArrayFieldProps): JSX.Element => {
 
   return (
     <>
-      <div className={`array-input-field-container ${name}`}>
-        {name.includes('array_disabled') ?
-          <InputSwitch
-            {...props}
-            type={'text'}
-            name={name}
-          />
-          :
-
-          <div className='input-items-container-new'>
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-              <ChaiseDroppable droppableId={`${name}-input-items-new`}>
-                {
-                  (provided: DroppableProvided) => (
-                    <ul
-                      className={`input-items-new ${fields.length ? 'add-margin-bottom' : ''}`}
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                      key={`${name}-list`}
-                    >
-                      {fields.map((item: object & { id: string }, index: number) => DraggableItemRenderer(item, index, disableInput))}
-                      {provided.placeholder}
-                    </ul>
-                  )
-                }
-              </ChaiseDroppable>
-            </DragDropContext>
-            <div className={`add-element-container ${baseArrayType}`}>
+      <div className={`array-input-field-container ${name} ${getInputType({ name: baseArrayType })}`}>
+        <div className='input-items-container-new'>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <ChaiseDroppable droppableId={`${name}-input-items-new`}>
+              {
+                (provided: DroppableProvided) => (
+                  <ul
+                    className={'input-items-new'}
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    key={`${name}-list`}
+                  >
+                    {fields.map((item: object & { id: string }, index: number) => DraggableItemRenderer(item, index, disableInput))}
+                    {provided.placeholder}
+                  </ul>
+                )
+              }
+            </ChaiseDroppable>
+          </DragDropContext>
+          {(!disableInput || true) &&
+            <div className={`add-element-container ${getInputType({ name: baseArrayType })} ${!disableInput ? 'add-padding-bottom' : ''} ${fields.length ? 'add-margin-top' : ''}`}>
               <InputSwitch
                 {...props}
+                requiredInput={requiredInput && !fields.length}
                 type={getInputType({ name: baseArrayType })}
-                {...register(`${name}-new-item`, { value: '', onChange: () => trigger(`${name}-new-item`) })}
+                {...register(`${name}-new-item`, {
+                  required: requiredInput && !fields.length,
+                  value: '',
+                  onChange: () => trigger(`${name}-new-item`)
+                })}
                 displayExtraDateTimeButtons={true}
                 displayDateTimeLabels={baseArrayType === 'date' ? false : true}
               />
               <button
                 type='button' className='chaise-btn chaise-btn-secondary chaise-btn-sm add-button'
-                onClick={() => addItem(addNewValue)}
+                onClick={() => {
+                  addItem(addNewValue)
+                  clearAddNewField()
+                }}
                 /**
                  * We disable the Add button when -
                  * 1. There are validation errors in the addNewValue field.
@@ -169,13 +171,11 @@ const ArrayField = (props: ArrayFieldProps): JSX.Element => {
                 disabled={Object.keys(formState.errors).includes(`${name}-new-item`) || (typeof addNewValue === 'boolean' ? false : !addNewValue)}
               >Add</button>
             </div>
-          </div>
-
-        }
+          }
+        </div>
       </div>
     </>
   )
 }
 
 export default React.memo(ArrayField);
-
