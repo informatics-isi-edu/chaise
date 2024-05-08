8678eeb5f8357be740e81b5444c69af79e9f1f1d
Josh Chudy <jchudy@isi.edu>
2024-05-08 11:42:46 -0700

    migrate foreign-key-dropdown spec (#2455)
    * migrate foreign-key-dropdown spec. Add more special classes for aligning content (deriva-webapps labels for user controls) and other classes for use in markdown containers to align some custom content above iframes
    * update class name. For positioning classes, separate middle/center for vertical/horizontal

--- src/components/input-switch/foreignkey-dropdown-field.tsx
+++ src/components/input-switch/foreignkey-dropdown-field.tsx
@@ -509,7 +509,10 @@ const ForeignkeyDropdownField = (props: ForeignkeyDropdownFieldProps): JSX.Eleme
                 </button>
               </div>}
             </Dropdown.Toggle>
-            {!props.disableInput && dropdownReference && <Dropdown.Menu className='responsive-dropdown-menu' ref={dropdownMenuRef}>
+            {/* when there are very few columns in recordedit, foreign key dropdowns were opening 
+              up causing them to open "behind" the top container and making some options unclickable
+              https://github.com/informatics-isi-edu/chaise/pull/2455 */}
+            {!props.disableInput && dropdownReference && <Dropdown.Menu flip={false} className='responsive-dropdown-menu' ref={dropdownMenuRef}>
               <li className='search-row'>
                 <SearchInput
                   initialSearchTerm=''
